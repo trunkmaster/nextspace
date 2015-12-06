7034188ad9c3cee38a4669227143ba07659a16d8
stoyan <stoyan255@ukr.net>
2015-12-06 02:09:53 +0200

    - Preferences.app; Power module prototype was added; - Preferences.app: Password module protorype enabled; - Preferences.app: Display module split into Display and Screen modules. Screen module holds monitors' arrangement settings. Dimming and swtching off displays moved into Power module.

---
+++ Applications/Preferences/Modules/Screen/Screen.m
@@ -0,0 +1,101 @@
+/*
+  Screen.m
+
+  Controller class for Screen preferences bundle
+
+  Author:	Sergii Stoian <stoyan255@ukr.net>
+  Date:		28 Nov 2015
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public
+  License along with this program; if not, write to:
+
+  Free Software Foundation, Inc.
+  59 Temple Place - Suite 330
+  Boston, MA  02111-1307, USA
+*/
+#import <AppKit/NSApplication.h>
+#import <AppKit/NSNibLoading.h>
+#import <AppKit/NSView.h>
+#import <AppKit/NSBox.h>
+#import <AppKit/NSImage.h>
+#import <AppKit/NSButton.h>
+#import <AppKit/NSTableView.h>
+#import <AppKit/NSTableColumn.h>
+#import <AppKit/NSBrowser.h>
+#import <AppKit/NSMatrix.h>
+
+#import "Screen.h"
+
+@implementation Screen
+
+static NSBundle                 *bundle = nil;
+static NSUserDefaults           *defaults = nil;
+static NSMutableDictionary      *domain = nil;
+
+- (id)init
+{
+  self = [super init];
+  
+  defaults = [NSUserDefaults standardUserDefaults];
+  domain = [[defaults persistentDomainForName:NSGlobalDomain] mutableCopy];
+
+  bundle = [NSBundle bundleForClass:[self class]];
+  NSString *imagePath = [bundle pathForResource:@"Screen" ofType:@"tiff"];
+  image = [[NSImage alloc] initWithContentsOfFile:imagePath];
+      
+  return self;
+}
+
+- (void)dealloc
+{
+  [image release];
+  [super dealloc];
+}
+
+- (void)awakeFromNib
+{
+  [view retain];
+  [window release];
+}
+
+- (NSView *)view
+{
+  if (view == nil)
+    {
+      if (![NSBundle loadNibNamed:@"Screen" owner:self])
+        {
+          NSLog (@"Screen.preferences: Could not load NIB, aborting.");
+          return nil;
+        }
+    }
+  
+  return view;
+}
+
+- (NSString *)buttonCaption
+{
+  return @"Screen Preferences";
+}
+
+- (NSImage *)buttonImage
+{
+  return image;
+}
+
+//
+// Action methods
+//
+
+@end
+
