b482b8b18f64139e45924dd3581cacd9b93ec6ca
Stoyan <stoyan255@ukr.net>
2016-02-05 18:44:58 +0200

    Preferences-Screen: implement display boxes dragging and selection.

--- Applications/Preferences/Modules/Screen/Screen.m
+++ Applications/Preferences/Modules/Screen/Screen.m
@@ -36,6 +36,12 @@
 #import <AppKit/NSMatrix.h>
 #import <AppKit/NSGraphics.h>
 
+#import <AppKit/PSOperators.h>
+#import <AppKit/NSEvent.h>
+#import <AppKit/NSWindow.h>
+
+#import <NXSystem/NXDisplay.h>
+
 #import "Screen.h"
 
 @implementation ScreenPreferences
@@ -102,6 +108,13 @@ static NSMutableDictionary      *domain = nil;
 //
 // Action methods
 //
+- (void)displayBoxClicked:(DisplayBox *)sender
+{
+  for (DisplayBox *db in displayBoxList)
+    {
+      [sender setSelected:(db == sender) ? YES : NO];
+    }
+}
 
 //
 // Helper methods
@@ -143,12 +156,13 @@ static NSMutableDictionary      *domain = nil;
   for (NXDisplay *d in displays)
     {
       displayRect = [d frame];
-      displayRect.origin.x = floor(displayRect.origin.x*scaleFactor)+0.5;
-      displayRect.origin.y = floor(displayRect.origin.y*scaleFactor)+0.5;
+      displayRect.origin.x = floor(displayRect.origin.x*scaleFactor)+0.4;
+      displayRect.origin.y = floor(displayRect.origin.y*scaleFactor)-0.3;
       displayRect.size.width = floor(displayRect.size.width*scaleFactor);
       displayRect.size.height = floor(displayRect.size.height*scaleFactor);
 
       dBox = [[DisplayBox alloc] initWithFrame:displayRect];
+      [dBox setName:[d outputName]];
       [displayBoxList addObject:dBox];
       [canvas addSubview:dBox];
     }
@@ -164,7 +178,8 @@ static NSMutableDictionary      *domain = nil;
   
   xOffset = (sRect.size.width - (screenSize.width * scaleFactor))/2;
   // Align boxes at that top edge
-  yOffset = sRect.size.height - (sRect.size.height - (screenSize.height * scaleFactor))/2;
+  yOffset = sRect.size.height -
+    (sRect.size.height - (screenSize.height * scaleFactor))/2;
   
   for (DisplayBox *dBox in displayBoxList)
     {
@@ -205,28 +220,169 @@ static NSMutableDictionary      *domain = nil;
 
 - initWithFrame:(NSRect)frameRect
 {
+  NSRect nameRect;
+  
   self = [super initWithFrame:frameRect];
   [self setBorderType:NSLineBorder];
   [self setTitlePosition:NSNoTitle];
   [self setContentViewMargins:NSMakeSize(1, 1)];
 
+  nameRect = frameRect;
+  nameRect.size.height = 15;
+  nameRect.origin.x = 0;
+  nameRect.origin.y = (frameRect.size.height - nameRect.size.height)/2;
+  nameField = [[NSTextField alloc] initWithFrame:nameRect];
+  [nameField setEditable:NO];
+  [nameField setSelectable:NO];
+  [nameField setDrawsBackground:NO];
+  [nameField setTextColor:[NSColor whiteColor]];
+  [nameField setAlignment:NSCenterTextAlignment];
+  [nameField setBezeled:NO];
+
+  [self addSubview:nameField];
+  [nameField release];
+  
+  isMainDisplay = NO;
+
   return self;
+}
+
+- (void)setName:(NSString *)name
+{
+  [nameField setStringValue:name];
 }
 
 - (void)setMainDisplay:(BOOL)isMain
 {
+  isMainDisplay = YES;
+  [[self superview] setNeedsDisplay:YES];
 }
 
-- (void)mouseDown
+- (void)setSelected:(BOOL)selected
 {
+  isSelected = selected;
+  [self setNeedsDisplay:YES];
+}
+
+- (void)mouseDown:(NSEvent *)theEvent
+{
+  NSLog(@"DisplayBox: mouseDown");
+  // [ displayBoxClicked:self];
+  
+  if ([theEvent clickCount] >= 2)
+    {
+      // Set main display
+    }
+  else
+    {
+      NSWindow   *window = [self window];
+      NSView     *superview = [self superview];
+      NSRect     superFrame = [superview frame];
+      NSRect     boxRect = [self frame];
+      NSPoint    boxOrigin = boxRect.origin;
+      NSPoint    lastLocation;
+      NSPoint    location, initialLocation;
+      NSUInteger eventMask = (NSLeftMouseDownMask | NSLeftMouseUpMask
+                              | NSPeriodicMask | NSOtherMouseUpMask
+                              | NSRightMouseUpMask);
+      NSDate     *theDistantFuture = [NSDate distantFuture];
+      BOOL       done = NO;
+
+      initialLocation = lastLocation = [theEvent locationInWindow];
+      [NSEvent startPeriodicEventsAfterDelay:0.02 withPeriod:0.02];
+
+      while (!done)
+        {
+          theEvent = [NSApp nextEventMatchingMask:eventMask
+                                        untilDate:theDistantFuture
+                                           inMode:NSEventTrackingRunLoopMode
+                                          dequeue:YES];
+
+          switch ([theEvent type])
+            {
+            case NSRightMouseUp:
+            case NSOtherMouseUp:
+            case NSLeftMouseUp:
+              /* right mouse up or left mouse up means we're done */
+              NSLog(@"Mouse UP.");
+              done = YES;
+              break;
+            case NSPeriodic:
+              location = [window mouseLocationOutsideOfEventStream];
+              if (NSEqualPoints(location, lastLocation) == NO)
+                {
+                  boxOrigin.x += (location.x - lastLocation.x);
+                  if (boxOrigin.x < 0)
+                    {
+                      boxOrigin.x = 0;
+                    }
+                  else if ((boxOrigin.x + boxRect.size.width)
+                           > superFrame.size.width)
+                    {
+                      boxOrigin.x = superFrame.size.width - boxRect.size.width;
+                    }
+                  
+                  boxOrigin.y += (location.y - lastLocation.y);
+                  if (boxOrigin.y < 0)
+                    {
+                      boxOrigin.y = 0;
+                    }
+                  else if ((boxOrigin.y + boxRect.size.height)
+                           > superFrame.size.height)
+                    {
+                      boxOrigin.y = superFrame.size.height - boxRect.size.height;
+                    }
+                  
+                  [self setFrameOrigin:boxOrigin];
+                  [superview setNeedsDisplay:YES];
+                  
+                  lastLocation = location;
+                }
+              break;
+
+            default:
+              break;
+            }
+        }
+      [NSEvent stopPeriodicEvents];
+
+      if (NSEqualPoints(initialLocation, lastLocation) == YES)
+        {
+          [self setSelected:YES];
+        }
+    }  
 }
 
 - (void)drawRect:(NSRect)rect
 {
-  [super drawRect:rect];
+  NSColor *desktopColor;
   
-  [_fill_color set];
+  [super drawRect:rect];
+
+  desktopColor = [NSColor colorWithDeviceRed:83.0/255.0
+                                       green:83.0/255.0
+                                        blue:116.0/255.0
+                                       alpha:1];
+  [desktopColor set];
   NSRectFill([[self contentView] frame]);
+
+  // Draw red frame
+  if (isSelected)
+    {
+      [[NSColor yellowColor] set];
+      PSnewpath();
+      PSmoveto(1,1);
+      PSlineto(1, rect.size.height-2);
+      PSlineto(rect.size.width-2, rect.size.height-2);
+      PSlineto(rect.size.width-2, 1);
+      PSlineto(1, 1);
+      PSstroke();
+    }
+  
+  if (isMainDisplay)
+    {
+      // Draw dock and icon yard
+    }
 }
 
 @end
