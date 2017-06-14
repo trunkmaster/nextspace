bfed7116587580e783e54c31f692d55c1ee9ad39
Stoyan <stoyan255@ukr.net>
2017-06-14 12:39:00 +0300

    * some additions to WindowMaker tree

---
+++ Applications/Workspace/WindowMaker/config-paths.h
@@ -0,0 +1,13 @@
+/* this is a generated file - do not edit */
+
+/* define where the translations are stored */
+#define LOCALEDIR "/usr/NextSpace/Apps/Workspace.app"
+
+/* define an extra path for pixmaps */
+#define PIXMAPDIR "/usr/NextSpace/Apps/Workspace.app/WindowMaker/Pixmaps"
+
+/* where shared data is stored */
+#define PKGDATADIR "/usr/NextSpace/Apps/Workspace.app/WindowMaker"
+
+/* where the configuration is stored */
+#define SYSCONFDIR "/usr/NextSpace/Apps/Workspace.app"
