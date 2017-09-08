b8166caa298134dd68e6d7304660d2f3d3525a30
Sergii Stoian <stoyan255@ukr.net>
2017-09-09 02:20:08 +0300

    * Update copyrights, add comments.

--- Applications/Terminal/Controller.h
+++ Applications/Terminal/Controller.h
@@ -1,6 +1,11 @@
 /*
- *
- */
+  Copyright (C) 2015-2017 Sergii Stoian <stoyan255@ukr.net>
+
+  This file is a part of Terminal.app. Terminal.app is free software; you
+  can redistribute it and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software Foundation; version 2
+  of the License. See COPYING or main.m for more information.
+*/
 
 #import <AppKit/AppKit.h>
 
