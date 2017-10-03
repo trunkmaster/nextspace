894bab1cd1aa4a5f87911a829d0f13d87613fbb5
Stoyan <stoyan255@ukr.net>
2017-10-03 19:22:09 +0300

    * Terminal: "Add Arguments" panel enhancements.

--- Applications/Terminal/TerminalServices.m
+++ Applications/Terminal/TerminalServices.m
@@ -284,18 +284,19 @@ static NSDictionary *servicesDictionary = nil;
       }
     cmdline = [str autorelease];
 
-    // if (p_pos!=-1)
-    //   {
-    //     cmdline = [TerminalServicesParameterWindowController
-    //     			getCommandlineFrom:cmdline
-    //                                    selectRange:NSMakeRange(p_pos,2)
-    //                                        service:name];
-    //     if (!cmdline)
-    //       {
-    //         *error=[_(@"Service aborted by user.") retain];
-    //         return;
-    //       }
-    //   }
+    // "Add Arguments" panel will be shown
+    if (p_pos != -1)
+      {
+        cmdline = [self	getCommandlineFrom:cmdline
+                               selectRange:NSMakeRange(p_pos,2)
+                                   service:name];
+        NSLog(@"Command line now is: %@", cmdline);
+        if (!cmdline)
+          {
+            *error=[_(@"Service aborted by user.") retain];
+            return;
+          }
+      }
     
     // No Shell/Default shell
     if (shell)
@@ -454,137 +455,45 @@ static NSDictionary *servicesDictionary = nil;
 
 @implementation TerminalServices (AddArguments)
 
--(void)_cancel:(id)sender
+- (void)cancel:(id)sender
 {
   [NSApp stopModalWithCode:NSRunAbortedResponse];
 }
 
--(void)_ok:(id)sender
+- (void)ok:(id)sender
 {
   [NSApp stopModalWithCode:NSRunStoppedResponse];
 }
 
-
-- initWithCommandline: (NSString *)cmdline
-          selectRange: (NSRange)r
-              service: (NSString *)service_name
-{
-  NSWindow *win;
-
-  win=[[NSPanel alloc] initWithContentRect: NSMakeRect(100,100,380,100)
-                                 styleMask: NSTitledWindowMask|NSResizableWindowMask
-                                   backing: NSBackingStoreRetained
-                                     defer: YES];
-  if (!(self=[super initWithWindow: win])) return nil;
-
-  {
-    GSVbox *vbox;
-
-    vbox=[[GSVbox alloc] init];
-    [vbox setBorder: 4];
-    [vbox setDefaultMinYMargin: 4];
-
-    {
-      NSButton *b;
-      GSHbox *hbox;
-
-      hbox=[[GSHbox alloc] init];
-      [hbox setDefaultMinXMargin: 4];
-      [hbox setAutoresizingMask: NSViewMinXMargin];
-
-      b=[[NSButton alloc] init];
-      [b setTitle: _(@"Cancel")];
-      [b setTarget: self];
-      [b setAction: @selector(_cancel:)];
-      [b sizeToFit];
-      [hbox addView: b];
-      [b release];
-
-      b=[[NSButton alloc] init];
-      [b setTitle: _(@"OK")];
-      [b setKeyEquivalent: @"\r"];
-      [b setTarget: self];
-      [b setAction: @selector(_ok:)];
-      [b sizeToFit];
-      [hbox addView: b];
-      [b release];
-
-      [vbox addView: hbox  enablingYResizing: NO];
-      [hbox release];
-    }
-
-    {
-      NSTextField *f;
-
-      tf_cmdline=f=[[NSTextField alloc] init];
-      [f setAutoresizingMask: NSViewWidthSizable];
-      [f sizeToFit];
-      [f setStringValue: cmdline];
-      [vbox addView: f  enablingYResizing: NO];
-      DESTROY(f);
-
-      f=[[NSTextField alloc] init];
-      [f setAutoresizingMask: NSViewMaxXMargin];
-      [f setStringValue: _(@"Command line:")];
-      [f setEditable: NO];
-      [f setDrawsBackground: NO];
-      [f setBordered: NO];
-      [f setBezeled: NO];
-      [f setSelectable: NO];
-      [f sizeToFit];
-      [f setAutoresizingMask: 0];
-      [vbox addView: f  enablingYResizing: NO];
-      DESTROY(f);
-
-      [tf_cmdline setTarget: self];
-      [tf_cmdline setAction: @selector(_ok:)];
-    }
-
-    [win setContentView: vbox];
-    [vbox release];
-  }
-  [win setDelegate: self];
-  [win setTitle:
-         [NSString stringWithFormat: _(@"Parameter for service %@"),
-                   service_name]];
-
-  [win autoSetupKeyViewChain];
-  [win makeFirstResponder: tf_cmdline];
-  /*	if (r.length)
-	{
-        NSText *t=[win fieldEditor: NO  forObject: tf_cmdline];
-        printf("t=%@ r=%@\n",[t text],NSStringFromRange(r));
-        [t setSelectedRange: r];
-	}*/
-
-  [win release];
-
-  return self;
-}
-
-
--(NSString *) _cmdline
-{
-  return [[[tf_cmdline stringValue] retain] autorelease];
-}
-
-
-+ (NSString *)getCommandlineFrom:(NSString *)cmdline
+- (NSString *)getCommandlineFrom:(NSString *)cmdline
                      selectRange:(NSRange)r
                          service:(NSString *)service_name
 {
-  TerminalServicesParameterWindowController *wc;
   NSString *s;
   int result;
 
-  wc = [[self alloc] initWithCommandline:cmdline
-                             selectRange:r
-                                 service:service_name];
-  [wc showWindow:self];
-  result = [NSApp runModalForWindow:[wc window]];
-  s = [wc _cmdline];
-  [wc close];
-  DESTROY(wc);
+  if (addArgsPanel == nil)
+    {
+      if ([NSBundle loadNibNamed:@"AddArguments" owner:self] == NO)
+        {
+          NSLog(@"Error loading NIB AddArguments");
+          return nil;
+        }
+    }
+
+  [commandField setStringValue:cmdline];
+  if (r.length)
+    {
+      NSText *text = [addArgsPanel fieldEditor:NO forObject:commandField];
+      // printf("t=%@ r=%@\n",[t text],NSStringFromRange(r));
+      [text setSelectedRange:r];
+    }
+  [serviceNameField setStringValue:service_name];
+  [addArgsPanel makeKeyAndOrderFront:self];
+  
+  result = [NSApp runModalForWindow:addArgsPanel];
+  s = [commandField stringValue];
+  [addArgsPanel close];
 
   if (result == NSRunStoppedResponse)
     return s;
