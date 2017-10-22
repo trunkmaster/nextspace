6395843b5110928e845e71c25638b3692b3d9ad3
Sergii Stoian <stoyan255@ukr.net>
2017-10-22 03:24:10 +0300

    * Frameworks/NXSystem: NXKeyboard class added - X Window specific methods went here. * Preferences: Use NXKeyboard methods for Key Repeat manipulation.

---
+++ Frameworks/NXSystem/NXKeyboard.h
@@ -0,0 +1,56 @@
+/*
+  Class:               NXKeyboard
+  Inherits from:       NSObject
+  Class descritopn:    Keyboard configuration manipulation (type, rate, layouts)
+
+  Copyright (C) 2017 Sergii Stoian <stoyan255@ukr.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#import <Foundation/Foundation.h>
+#import <NXFoundation/NXDefaults.h>
+
+#define XKB_BASE_LST @"/usr/share/X11/xkb/rules/base.lst"
+
+@interface NXKeyboard : NSObject
+{
+  NSDictionary *layoutDict;
+  NSDictionary *modelDict;
+  NSDictionary *variantDict;
+  NSDictionary *optionDict;
+}
+
+- (void)configureWithDefaults:(NXDefaults *)defs;
+
+- (NSDictionary *)modelList;
+- (NSString *)model;
+- (void)setModel:(NSString *)name;
+
+- (NSDictionary *)layoutList;
+- (NSString *)layout;
+- (void)addLayout:(NSString *)name;
+- (void)removeLayout:(NSString *)name;
+- (void)setLayoutList:(NSArray *)layouts variants:(NSArray *)variants;
+// Return list of dictionaries with keys: Layout, Desc.
+- (NSDictionary *)variantsForLayout:(NSString *)layout;
+
+
+- (NSInteger)initialRepeat;
+- (void)setInitialRepeat:(NSInteger)delay;
+- (NSInteger)repeatRate;
+- (void)setRepeatRate:(NSInteger)rate;
+
+@end
