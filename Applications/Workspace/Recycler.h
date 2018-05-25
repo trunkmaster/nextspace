624c40f3960a80390fbb2a13945e939a5586c9ed
Sergii Stoian <stoyan255@ukr.net>
2018-05-25 19:12:17 +0300

    Workspace: (Recycler): refactoring - added new controller class Recycler; RecyclerIcon is a appicon window, RecyclerIconVew - content view of RecyclerIcon; all management moved to Recycler class; initial implementation of Recycler window.

--- Applications/Workspace/Recycler.h
+++ Applications/Workspace/Recycler.h
@@ -1,22 +1,48 @@
 /* All Rights reserved */
 
 #include <AppKit/AppKit.h>
+#import <NXAppKit/NXIconView.h>
+
 #import "Workspace+WindowMaker.h"
 
-@interface RecyclerView : NSView
+@interface RecyclerIconView : NSView
 - (void)setImage:(NSImage *)anImage;
 @end
 
 @interface RecyclerIcon : NSWindow
 {
-  WAppIcon     *dockIcon;
-  RecyclerView *view;
 }
 
 + (WAppIcon *)createAppIconForDock:(WDock *)dock;
 + (WAppIcon *)recyclerAppIconForDock:(WDock *)dock;
   
+// - initWithDock:(WDock *)dock;
+// - (WAppIcon *)dockIcon;
+// - (NSImage *)iconImage;
+// - (void)updateIconImage;
+
+@end
+
+@interface Recycler : NSObject
+{
+  RecyclerIcon     *appIcon;
+  RecyclerIconView *appIconView;
+
+  WAppIcon         *dockIcon;
+  NSImage          *iconImage;
+  NSString         *recyclerPath;
+
+  // Panel
+  NSPanel      *panel;
+  NSImageView  *panelIcon;
+  NSScrollView *panelView;
+  NXIconView   *filesView;
+}
+
 - initWithDock:(WDock *)dock;
 - (WAppIcon *)dockIcon;
+- (RecyclerIcon *)appIcon;
+
+- (void)mouseDown:(NSEvent*)theEvent;
 
 @end
