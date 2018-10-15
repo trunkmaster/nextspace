73d5fa27ce88d7b0c1d29c5f918e1ea98a0fca16
Sergii Stoian <stoyan255@gmail.com>
2018-10-16 02:00:39 +0300

    NXAppKit:NXShortenString(): cleanup and formatting.

--- Frameworks/NXAppKit/NXUtilities.m
+++ Frameworks/NXAppKit/NXUtilities.m
@@ -21,80 +21,66 @@ NSString *NXShortenString(NSString *fullString,
       return fullString;
     }
 
-  while ([font widthOfString:shortString] > viewWidth)
-    {
-      if (dotsPosition == NXDotsAtLeft)
-        {
-          if (elementType == NXSymbolElement)
-            {
-              [shortString deleteCharactersInRange:NSMakeRange(0, 1)];
-            }
-          else if (elementType == NXPathElement)
-            {
-              pathComponents = [[shortString pathComponents] mutableCopy];
-              [pathComponents removeObjectAtIndex:0];
-              shortString = [[NSString pathWithComponents:pathComponents] 
-                              mutableCopy];
-              [pathComponents release];
-            }
-          else // NXWordElement
-            {
-              // TODO
-            }
-        }
-      else if (dotsPosition == NXDotsAtRight)
-        {
-          if (elementType == NXSymbolElement)
-            {
-              [shortString 
+  while ([font widthOfString:shortString] > viewWidth) {
+    switch (dotsPosition) {
+    case NXDotsAtLeft:
+      if (elementType == NXSymbolElement) {
+        [shortString deleteCharactersInRange:NSMakeRange(0, 1)];
+      }
+      else if (elementType == NXPathElement) {
+        pathComponents = [[shortString pathComponents] mutableCopy];
+        [pathComponents removeObjectAtIndex:0];
+        shortString = [[NSString pathWithComponents:pathComponents] 
+                        mutableCopy];
+        [pathComponents release];
+      }
+      else { // NXWordElement
+        // TODO
+      }
+      break;
+    case NXDotsAtRight:
+      if (elementType == NXSymbolElement) {
+        [shortString 
                 deleteCharactersInRange:NSMakeRange([shortString length]-1, 1)];
-            }
-          else if (elementType == NXPathElement)
-            {
-              shortString = [NSMutableString 
+      }
+      else if (elementType == NXPathElement) {
+        shortString = [NSMutableString 
                               stringWithString:
-                                [shortString stringByDeletingLastPathComponent]];
-            }
-          else // NXWordElement
-            {
-              // TODO
-            }
-        }
-      else // NXDotsAtCenter
-        {
-          // TODO
-        }
+                          [shortString stringByDeletingLastPathComponent]];
+      }
+      else { // NXWordElement
+        // TODO
+      }
+      break;
+    case NXDotsAtCenter:
+    default:
+      // TODO
+      break;
     }
+  }
 
   // String was shortened - insert dots
-  if (dotsPosition == NXDotsAtLeft)
-    {
-      if (elementType == NXSymbolElement ||
-          elementType == NXWordElement)
-        {
-          return [NSString stringWithFormat:@"...%@", shortString];
-        }
-      else if (elementType == NXPathElement)
-        {
-          return [NSString stringWithFormat:@".../%@", shortString];
-        }
+  if (dotsPosition == NXDotsAtLeft) {
+    if (elementType == NXSymbolElement ||
+        elementType == NXWordElement) {
+      return [NSString stringWithFormat:@"...%@", shortString];
     }
-  else if (dotsPosition == NXDotsAtRight)
-    {
-      if (elementType == NXSymbolElement ||
-          elementType == NXWordElement)
-        {
-          return [NSString stringWithFormat:@"%@...", shortString];
-        }
-      else if (elementType == NXPathElement)
-        {
-          return [NSString stringWithFormat:@"%@/...", shortString];
-        }
+    else if (elementType == NXPathElement) {
+      return [NSString stringWithFormat:@".../%@", shortString];
     }
-  else // NXDotsAtCenter
-    {
-      // TODO
+  }
+  else if (dotsPosition == NXDotsAtRight) {
+    if (elementType == NXSymbolElement ||
+        elementType == NXWordElement) {
+      return [NSString stringWithFormat:@"%@...", shortString];
     }
+    else if (elementType == NXPathElement) {
+      return [NSString stringWithFormat:@"%@/...", shortString];
+    }
+  }
+  else { // NXDotsAtCenter
+    // TODO
+  }
 
   return fullString;
 }
