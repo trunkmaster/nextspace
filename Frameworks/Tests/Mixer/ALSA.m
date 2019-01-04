4ffe7f540359f4778a39907608894f34be5ee19c
Sergii Stoian <stoyan255@gmail.com>
2019-01-04 02:55:36 +0200

    Mixer: ALSA updates.

--- Frameworks/Tests/Mixer/ALSA.m
+++ Frameworks/Tests/Mixer/ALSA.m
@@ -5,9 +5,7 @@
 #import "ALSA.h"
 #import "ALSAElement.h"
 
-static snd_mixer_t      *mixer;
-static char		*mixer_name;
-static BOOL             mixerOpened = NO;
+static BOOL mixerOpened = NO;
 
 /* Convenience macro to die in informational manner.  */
 #define DIE(msg) {                                                     \
@@ -30,7 +28,11 @@ static BOOL             mixerOpened = NO;
 }
 - (void)addElement:(ALSAElement *)element
 {
-  [[element view] setFrameOrigin:NSMakePoint()];
+  NSRect laeFrame = [lastAddedElement frame];
+  NSRect eFrame = [[element view] frame];
+
+  eFrame.origin.y = laeFrame.origin.y + laeFrame.size.height;
+  [[element view] setFrameOrigin:eFrame.origin];
   [super addSubview:[element view]
          positioned:NSWindowAbove
          relativeTo:lastAddedElement];
@@ -40,38 +42,77 @@ static BOOL             mixerOpened = NO;
 
 @implementation ALSA
 
-- (void)dealloc
-{
-  snd_mixer_detach(mixer, mixer_name);
-  snd_mixer_close(mixer);
-  
-  [super dealloc];
-}
-
 struct card {
   struct card	*next;
   int		index;
-  char		*indexstr;
   char		*name;
   char		*device_name;
+  char		*chip_name;
 };
-static struct card first_card;
+
+- (snd_mixer_t *)_openMixerWithName:(char *)mixer_name
+{
+  snd_mixer_t *mixer;
+  
+  if (snd_mixer_open(&mixer, 0) < 0)
+    DIE(@"Cannot open mixer.");
+  if (snd_mixer_attach(mixer, mixer_name) < 0)
+    DIE(@"Cannot attach mixer.");
+  if (snd_mixer_selem_register(mixer, NULL, NULL) < 0)
+    DIE (@"Cannot register the mixer elements.");
+  if (snd_mixer_load(mixer) < 0)
+    DIE (@"Cannot load mixer.");
+
+  // [NSTimer scheduledTimerWithTimeInterval:0.5
+  //                                  target:self
+  //                                selector:@selector(refresh)
+  //                                userInfo:nil
+  //                                 repeats:YES];
+  // [NSApp setDelegate:self];
+  mixerOpened = YES;
+
+  return mixer;
+}
+
+- (void)_closeMixer:(snd_mixer_t *)mixer withName:(char *)mixer_name
+{
+  snd_mixer_detach(mixer, mixer_name);
+  snd_mixer_close(mixer);
+}
+
+struct element {
+  struct element   *next;
+  snd_mixer_elem_t *elem;
+  int              index;
+  char             *name;
+  int              playback_volume;
+  int              playback_volume_min;
+  int              playback_volume_max;
+  int              capture_volume;
+  struct {
+    int is_active:1;
+    int is_mono:1;
+    int is_playback:1;
+    int has_common_volume:1;
+  } flags;
+};
 
 - (struct card *)_enumerateCards
 {
   snd_ctl_card_info_t	*info;
-  struct card 		*card, *prev_card;
+  struct card		*first_card;
+  struct card 		*card, *prev_card = NULL;
   int 			count, number, err;
   char 			buf[16];
   snd_ctl_t		*ctl;
   
-  first_card.indexstr = "-";
-  first_card.name = "Default";
-  first_card.device_name = "default";
-  count = 1;
+  // first_card.indexstr = "-";
+  // first_card.name = "Default";
+  // first_card.device_name = "default";
+  count = 0;
 
   snd_ctl_card_info_alloca(&info);
-  prev_card = &first_card;
+  // prev_card = &first_card;
   number = -1;
   
   for (;;) {
@@ -93,52 +134,80 @@ static struct card first_card;
       continue;
     
     card = calloc(1, sizeof *card);
+    
     card->index = number;
-    sprintf(buf, "%d", number);
-    card->indexstr = strdup(buf);
     
     card->name = strdup(snd_ctl_card_info_get_name(info));
     sprintf(buf, "hw:%d", number);
     
     card->device_name = strdup(buf);
+
+    {
+      snd_mixer_t *mixer = [self _openMixerWithName:card->device_name];
+      snd_hctl_t *hctl;
+      snd_ctl_t *ctl;
+      snd_ctl_card_info_t *card_info;
+      snd_ctl_card_info_alloca(&card_info);
+      if (card->device_name)
+        err = snd_mixer_get_hctl(mixer, card->device_name, &hctl);
+      else
+        err = -1;
+      if (err >= 0) {
+        ctl = snd_hctl_ctl(hctl);
+        err = snd_ctl_card_info(ctl, card_info);
+        if (err >= 0) {
+          card->name = strdup(snd_ctl_card_info_get_mixername(card_info));
+        }
+      }
+      [self _closeMixer:mixer withName:card->device_name];
+    }
     
-    prev_card->next = card;
+    if (number == 0)
+      first_card = card;
+
+    if (prev_card)
+      prev_card->next = card;
+    
     prev_card = card;
     ++count;
   }
 
-  return &first_card;
+  return first_card;
 }
 
-- (void)_openMixer
+- (void)_enumerateElementsForCard:(struct card *)card
 {
-  if (snd_mixer_open(&mixer, 0) < 0)
-    DIE(@"Cannot open mixer.");
-  if (snd_mixer_attach(mixer, mixer_name) < 0)
-    DIE(@"Cannot attach mixer.");
-  if (snd_mixer_selem_register(mixer, NULL, NULL) < 0)
-    DIE (@"Cannot register the mixer elements.");
-  if (snd_mixer_load(mixer) < 0)
-    DIE (@"Cannot load mixer.");
-
-  // [NSTimer scheduledTimerWithTimeInterval:0.5
-  //                                  target:self
-  //                                selector:@selector(refresh)
-  //                                userInfo:nil
-  //                                 repeats:YES];
-  // [NSApp setDelegate:self];
-  mixerOpened = YES;
-}
-
-- (void)_closeMixer
-{
-  snd_mixer_detach(mixer, mixer_name);
-  snd_mixer_close(mixer);
+  snd_mixer_t          *mixer;
+  snd_mixer_elem_t     *elem;
+  snd_mixer_selem_id_t *selem_id;
+  
+  while (card) {
+    fprintf(stderr, "Mixer elements for `%s`:\n", card->device_name);
+    mixer = [self _openMixerWithName:card->device_name];
+  
+    snd_mixer_selem_id_alloca(&selem_id);
+  
+    for (elem = snd_mixer_first_elem(mixer); elem; elem = snd_mixer_elem_next(elem)) {
+      // controls_count += get_controls_count_for_elem(elem);
+      snd_mixer_selem_get_id(elem, selem_id);
+      fprintf(stderr, "\t%s (active:%i, capture:%i, common_switch:%i playback_switch:%i)\n",
+              snd_mixer_selem_id_get_name(selem_id),
+              snd_mixer_selem_is_active(elem),
+              snd_mixer_selem_has_capture_volume(elem),
+              snd_mixer_selem_has_common_switch(elem),
+              snd_mixer_selem_has_playback_switch(elem));
+    }
+    [self _closeMixer:mixer withName:card->device_name];
+    card = card->next;
+  }
 }
 
 - (void)awakeFromNib
 {
+  struct card *first_card;
   struct card *card = [self _enumerateCards];
+
+  first_card = card;
 
   [cardsList setRefusesFirstResponder:YES];
   [viewMode setRefusesFirstResponder:YES];
@@ -158,16 +227,14 @@ static struct card first_card;
   [elementsScroll setAutoresizingMask:(NSViewWidthSizable | NSViewHeightSizable)];
   [[alsaWindow contentView] addSubview:elementsScroll];
 
-  elementsView = [[ALSAElementsView alloc] initWithFrame:[elementsScroll documentVisibleRect]];
+  elementsView = [[ALSAElementsView alloc]
+                   initWithFrame:[elementsScroll documentVisibleRect]];
   [elementsScroll setDocumentView:elementsView];
 
-  [elementsView addElement:[[ALSAElement alloc] init]];
-  [elementsView addElement:[[ALSAElement alloc] init]];
+  // [elementsView addElement:[[ALSAElement alloc] init]];
+  // [elementsView addElement:[[ALSAElement alloc] init]];
 
-  // snd_mixer_elem_t *elem;
-  // for (elem = snd_mixer_first_elem(handle); elem; elem = snd_mixer_elem_next(elem)) {
-  //   controls_count += get_controls_count_for_elem(elem);
-  // }
+  [self _enumerateElementsForCard:first_card];
 } 
 
 - (void)showPanel
