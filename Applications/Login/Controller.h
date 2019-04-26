e7d15f5503e363b01c16b053f90e8aa32316ce70
Sergii Stoian <stoyan255@gmail.com>
2019-04-26 18:35:55 +0300

    Login: update source code files preamble.

--- Applications/Login/Controller.h
+++ Applications/Login/Controller.h
@@ -1,25 +1,23 @@
-/*
-   Controller.h
-   The main app controller of the Login application.
-
-   This file is part of xSTEP.
-
-   Copyright (C) 2005 
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+/* -*- mode: objc -*- */
+//
+// Project: Login
+//
+// Copyright (C) 2014-2019 Sergii Stoian
+//
+// This application is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This application is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free
+// Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+//
 
 #import <security/pam_appl.h>
 #import <X11/Xlib.h>
--- Applications/Login/LoginWindow.h
+++ Applications/Login/LoginWindow.h
@@ -1,36 +1,28 @@
-/* 
-   LoginWindow.h
-
-   Class to allow the display of a borderless window.  It also
-   provides the necessary functionality for some of the other nice
-   things we want the window to do.
-
-   Copyright (C) 2000 Free Software Foundation, Inc.
-
-   Author:  Gregory John Casamento <greg_casamento@yahoo.com>
-   Date: 2000
-   
-   This file is part of GNUstep.
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-   
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public
-   License along with this library; see the file COPYING.LIB.
-   If not, write to the Free Software Foundation,
-   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
-   You can reach me at:
-   Gregory Casamento, 14218 Oxford Drive, Laurel, MD 20707, 
-   USA
-*/
+/* -*- mode: objc -*- */
+//
+// Project: Login
+//
+// Description: Class to allow the display of a borderless window.  It also
+//              provides the necessary functionality for some of the other nice
+//              things we want the window to do.
+//
+// Copyright (C) 2000 Free Software Foundation, Inc.
+// Author: Gregory John Casamento <greg_casamento@yahoo.com>
+//
+// This application is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This application is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free
+// Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+//
 
 #import <AppKit/AppKit.h>
 
