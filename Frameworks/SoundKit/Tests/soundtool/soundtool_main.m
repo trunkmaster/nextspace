c1235dbbd558961e0563f16d97806f7cc96950cf
Sergii Stoian <stoyan255@gmail.com>
2019-06-19 03:26:36 +0300

    soundtool: more clean implementation to test sound play.

--- Frameworks/SoundKit/Tests/soundtool/soundtool_main.m
+++ Frameworks/SoundKit/Tests/soundtool/soundtool_main.m
@@ -13,16 +13,84 @@
 
 @interface SoundKitClient : NSObject
 {
-  SNDServer *server;
+  SNDServer     *server;
+  SNDPlayStream *stream;
+  
   BOOL          isRunning;
 }
 @end
 
-@implementation SoundKitClient (Play)
+@implementation SoundKitClient
+
+- (void)dealloc
+{
+  NSLog(@"SoundKitClient: dealloc");
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  fprintf(stderr, "\tRetain Count: %lu\n", [server retainCount]);
+  [server release];
+  [super dealloc];
+}
+
+- init
+{
+  self = [super init];
+
+  // 1. Connect to PulseAudio on locahost
+  server = [SNDServer sharedServer];
+  // 2. Wait for server to be ready
+  [[NSNotificationCenter defaultCenter]
+    addObserver:self
+       selector:@selector(serverStateChanged:)
+           name:SNDServerStateDidChangeNotification
+         object:server];
+  
+  return self;
+}
+
+// -----------------------------------------------------------------------------
+// --- Run loop
+// -----------------------------------------------------------------------------
+- (void)runLoopRun
+{
+  NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+  
+  isRunning = YES;
+  while (isRunning) {
+    // NSLog(@"RunLoop is running");
+    [runLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
+  }
+}
+
+- (void)runLoopStop
+{
+  isRunning = NO;
+}
+
+- (void)serverStateChanged:(NSNotification *)notif
+{
+  if (server.status == SNDServerReadyState) {
+    // {
+    //   fprintf(stderr, "========= Sound Server =========\n");
+    //   fprintf(stderr, "\t        Name: %s\n", [server.name cString]);
+    //   fprintf(stderr, "\t     Version: %s\n", [server.version cString]);
+    //   fprintf(stderr, "\t    Username: %s\n", [server.userName cString]);
+    //   fprintf(stderr, "\t    Hostname: %s\n", [server.hostName cString]);
+    //   fprintf(stderr, "\tRetain Count: %lu\n", [server retainCount]);
+    // }
+    // {
+    //   fprintf(stderr, "========= Default SoundOut =========\n");
+    //   [[server defaultOutput] printDescription];
+    // }
+    [self playSystemBeep];
+  }
+}
+
+// -----------------------------------------------------------------------------
+// --- Play
+// -----------------------------------------------------------------------------
 
 static SNDFILE        *snd_file;
 static pa_sample_spec sample_spec;
-static size_t         sample_length;
 
 int pa_sndfile_read_sample_spec(SNDFILE *sf, pa_sample_spec *ss)
 {
@@ -144,39 +212,34 @@ int pa_sndfile_read_channel_map(SNDFILE *sf, pa_channel_map *cm)
 
   return 0;
 }
-static void stream_write_callback(pa_stream *s, size_t length, void *userdata)
+
+- (void)playBytes:(NSNumber *)count
 {
-  sf_count_t l, l_read;
-  float      *d;
-  // pa_assert(s && length && snd_file);
+  size_t     length = [count unsignedIntValue];
+  sf_count_t bytes_to_read, bytes_read;
+  float      *buffer;
 
-  // fprintf(stderr, "stream_write_callback\n");
-
-  d = pa_xmalloc(length);
+  // NSLog(@"Play bytes count: %li", length);
+  buffer = pa_xmalloc(length);
 
   // pa_assert(sample_length >= length);
-  l = (sf_count_t) (length/pa_frame_size(&sample_spec));
-  l_read = sf_readf_float(snd_file, d, l);
-  fprintf(stderr, "length == %li l == %li l_read == %li\n", length, l, l_read);
+  bytes_to_read = (sf_count_t) (length/pa_frame_size(&sample_spec));
+  bytes_read = sf_readf_float(snd_file, buffer, bytes_to_read);
+  // fprintf(stderr, "Stream ready to receive: %li | Bytes to read: %li | Bytes were read: %li\n",
+  //         length, bytes_to_read, bytes_read);
   
-  if (l_read <= 0) {
-    pa_xfree(d);
-    fprintf(stderr, "End of file\n");
-    pa_stream_set_write_callback(s, NULL, NULL);
-    pa_stream_disconnect(s);
-    pa_stream_unref(s);
+  if (bytes_read <= 0) {
+    pa_xfree(buffer);
+    [stream deactivate];
     return;
   }
 
-  pa_stream_write(s, d, length, pa_xfree, 0, PA_SEEK_RELATIVE);
+  [stream playBuffer:buffer size:length tag:0];
 }
 
 - (void)playSystemBeep
 {
   struct SF_INFO sfi;
-  pa_channel_map channel_map;
-  /*  pa_proplist    *proplist;
-      pa_stream      *stream; */
   // char *file = "/usr/NextSpace/Sounds/Welcome-to-the-NeXT-world.snd";
   // char *file = "/usr/NextSpace/Sounds/SystemBeep.snd";
   char    *file = "/usr/NextSpace/Sounds/Rooster.snd";
@@ -196,173 +259,17 @@ static void stream_write_callback(pa_stream *s, size_t length, void *userdata)
   fprintf(stderr, "channels: %i rate: %i format: %i\n",
           sample_spec.channels, sample_spec.rate, sample_spec.format);
   sample_spec.format = PA_SAMPLE_FLOAT32LE;
-  // sample_spec.format = PA_SAMPLE_S16;
   
-  if (pa_sndfile_read_channel_map(snd_file, &channel_map) < 0) {
-    if (sample_spec.channels > 2) {
-      fprintf(stderr, "Warning: Failed to determine sample specification from file.\n");
-    }
-    pa_channel_map_init_extend(&channel_map, sample_spec.channels, PA_CHANNEL_MAP_DEFAULT);
-  }
-
-  sample_length = (size_t)sfi.frames * pa_frame_size(&sample_spec);
-
-  SNDStream *stream;
-  NSLog(@"Creating stream...");
-  stream = [[SNDPlayStream alloc] initOnDevice:nil
-                                      samplingRate:sample_spec.rate
-                                      channelCount:sample_spec.channels
-                                            format:sample_spec.format];
-  NSLog(@"Activating stream...");
+  // NSLog(@"Creating stream...");
+  stream = [[SNDPlayStream alloc] initOnDevice:[server defaultOutput]
+                                  samplingRate:sample_spec.rate
+                                  channelCount:sample_spec.channels
+                                        format:sample_spec.format];
+  [stream setDelegate:self];
+  [stream setAction:@selector(playBytes:)];
+  
+  // NSLog(@"Activate stream...");
   [stream activate];
-  // [stream playBuffer];
-  // NSLog(@"Deactivating stream...");
-  // [stream deactivate];
-  /*
-  // Create stream
-  proplist = pa_proplist_new();
-  pa_proplist_sets(proplist, PA_PROP_MEDIA_ROLE, "event");
-  stream = pa_stream_new_with_proplist(server.pa_ctx, "soundtool",
-                                       &sample_spec, &channel_map,
-                                       proplist);
-  // Connect default stream
-  pa_stream_connect_playback(stream, NULL, NULL, 0, NULL, NULL);
-  pa_stream_set_write_callback(stream, stream_write_callback, NULL);
-  */
-
-  // typedef struct pa_sample_spec {
-  //   pa_sample_format_t format;   /* The sample format */
-  //   uint32_t           rate;     /* The sample rate. (e.g. 44100) */
-  //   uint8_t            channels; /* Audio channels. (1 for mono, 2 for stereo, ...) */
-  // } pa_sample_spec;
-  // sample_spec.rate = 44100;
-  // sample_spec.channels = 2;
-  // sample_spec.format = ?;
-  
-  // typedef struct pa_channel_map {
-  //   uint8_t               channels;             /* Number of channels */
-  //   pa_channel_position_t map[PA_CHANNELS_MAX]; /* Channel labels */
-  // } pa_channel_map;
-  
-  // pa_stream* pa_stream_new(
-  //       pa_context *c                     /**< The context to create this stream in */,
-  //       const char *name                  /**< A name for this stream */,
-  //       const pa_sample_spec *ss          /**< The desired sample format */,
-  //       const pa_channel_map *map         /**< The desired channel map, or NULL for default */);
-  // play_stream = pa_stream_new(pa_ctx, "soundtool", &sample_spec, NULL);
-  
-  // int pa_stream_connect_playback(
-  //       pa_stream *s                  /**< The stream to connect to a sink */,
-  //       const char *dev               /**< Name of the sink to connect to, or NULL for default */ ,
-  //       const pa_buffer_attr *attr    /**< Buffering attributes, or NULL for default */,
-  //       pa_stream_flags_t flags       /**< Additional flags, or 0 for default */,
-  //       const pa_cvolume *volume      /**< Initial volume, or NULL for default */,
-  //       pa_stream *sync_stream        /**< Synchronize this stream with the specified one, or NULL for a standalone stream */);
-  // pa_stream_connect_playback(play_stream, "alsa_output.pci-0000_00_1b.0.analog-stereo", NULL, 0, NULL, NULL);
-}
-
-@end
-
-@implementation SoundKitClient
-
-- (void)dealloc
-{
-  NSLog(@"SoundKitClient: dealloc");
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
-  fprintf(stderr, "\tRetain Count: %lu\n", [server retainCount]);
-  [server release];
-  [super dealloc];
-}
-
-- init
-{
-  self = [super init];
-
-  // 1. Connect to PulseAudio on locahost
-  server = [SNDServer sharedServer];
-  // 2. Wait for server to be ready
-  [[NSNotificationCenter defaultCenter]
-    addObserver:self
-       selector:@selector(serverStateChanged:)
-           name:SKServerStateDidChangeNotification
-         object:server];
-  
-  return self;
-}
-
-- (void)runLoopRun
-{
-  NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
-  
-  isRunning = YES;
-  while (isRunning) {
-    // NSLog(@"RunLoop is running");
-    [runLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
-  }
-}
-
-- (void)runLoopStop
-{
-  isRunning = NO;
-}
-
-- (void)describeSoundSystem
-{
-  // Server
-  fprintf(stderr, "=== Sound Server ===\n");
-  fprintf(stderr, "\t        Name: %s\n", [server.name cString]);
-  fprintf(stderr, "\t     Version: %s\n", [server.version cString]);
-  fprintf(stderr, "\t    Username: %s\n", [server.userName cString]);
-  fprintf(stderr, "\t    Hostname: %s\n", [server.hostName cString]);
-  fprintf(stderr, "\tRetain Count: %lu\n", [server retainCount]);
-  
-  // // Sound Out
-  // fprintf(stderr, "=== SoundOut ===\n");
-  // for (SNDOut *sout in server.outputList) {
-  //   [sout printDescription];
-  // }
-}
-
-- (SNDOut *)defaultSoundOut
-{
-  SNDOut *sOut;
-  // SNDStream *sStream;
-  // sStream = [[SNDStream alloc] initWithDevice:sOut];
-
-  for (SNDOut *sout in [server outputList]) {
-    [sout printDescription];
-  }
-
-  sOut = [server defaultOutput];
-  fprintf(stderr, "========= Default SoundOut =========\n");
-  [sOut printDescription];
-
-  return sOut;
-}
-
-- (SNDIn *)defaultSoundIn
-{
-  SNDIn *sIn;
-
-  for (SNDIn *sin in [server inputList]) {
-    [sin printDescription];
-  }
-
-  sIn = [server defaultInput];
-  fprintf(stderr, "========= Default SoundIn =========\n");
-  [sIn printDescription];
-
-  return sIn;
-}
-
-- (void)serverStateChanged:(NSNotification *)notif
-{
-  if (server.status == SKServerReadyState) {
-    // [self describeSoundSystem];
-    [self defaultSoundOut];
-    // [self defaultSoundIn];
-    [self playSystemBeep];
-  }
 }
 
 @end
@@ -381,8 +288,8 @@ int main(int argc, char *argv[])
   NSConnection      *conn;
 
   client = [SoundKitClient new];
-  conn = [NSConnection defaultConnection];
-  [conn registerName:@"soundtool"];
+  // conn = [NSConnection defaultConnection];
+  // [conn registerName:@"soundtool"];
 
   signal(SIGHUP, handle_signal);
   signal(SIGINT, handle_signal);
@@ -393,7 +300,7 @@ int main(int argc, char *argv[])
 
   fprintf(stderr, "Runloop exited.\n");
 
-  [conn invalidate];
+  // [conn invalidate];
   [client release];
   [pool release];
 
