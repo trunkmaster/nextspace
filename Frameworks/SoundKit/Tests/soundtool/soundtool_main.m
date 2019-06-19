f3ebe34fd13a7e4e65d58c4115e22abe4f577a97
Sergii Stoian <stoyan255@gmail.com>
2019-06-20 01:12:50 +0300

    soundtool: simplify sound spec defining and remove unused code.

--- Frameworks/SoundKit/Tests/soundtool/soundtool_main.m
+++ Frameworks/SoundKit/Tests/soundtool/soundtool_main.m
@@ -26,9 +26,10 @@
 {
   NSLog(@"SoundKitClient: dealloc");
   [[NSNotificationCenter defaultCenter] removeObserver:self];
-  [stream release];
+  if (stream != nil)
+    [stream release];
   fprintf(stderr, "\tRetain Count: %lu\n", [server retainCount]);
-  [server release];
+  [server disconnect];
   [super dealloc];
 }
 
@@ -82,7 +83,13 @@
     //   fprintf(stderr, "========= Default SoundOut =========\n");
     //   [[server defaultOutput] printDescription];
     // }
-    [self playSystemBeep];
+    if ([self playSystemBeep] != 0) {
+      [self runLoopStop];
+    }
+  }
+  else if (server.status == SNDServerFailedState ||
+           server.status == SNDServerTerminatedState) {
+    [self runLoopStop];
   }
 }
 
@@ -92,127 +99,8 @@
 
 static SNDFILE        *snd_file;
 static pa_sample_spec sample_spec;
+static char           *file = NULL;
 
-int pa_sndfile_read_sample_spec(SNDFILE *sf, pa_sample_spec *ss)
-{
-  SF_INFO sfi;
-  int sf_errno;
-
-  // NSAssert(NULL, sf);
-  // NSAssert(NULL, ss);
-
-  // pa_zero(sfi);
-  if ((sf_errno = sf_command(sf, SFC_GET_CURRENT_SF_INFO, &sfi, sizeof(sfi)))) {
-    fprintf(stderr, "sndfile: %s", sf_error_number(sf_errno));
-    return -1;
-  }
-
-  switch (sfi.format & SF_FORMAT_SUBMASK) {
-
-  case SF_FORMAT_PCM_16:
-  case SF_FORMAT_PCM_U8:
-  case SF_FORMAT_PCM_S8:
-    ss->format = PA_SAMPLE_S16NE;
-    break;
-
-  case SF_FORMAT_PCM_24:
-    ss->format = PA_SAMPLE_S24NE;
-    break;
-
-  case SF_FORMAT_PCM_32:
-    ss->format = PA_SAMPLE_S32NE;
-    break;
-
-  case SF_FORMAT_ULAW:
-    ss->format = PA_SAMPLE_ULAW;
-    break;
-
-  case SF_FORMAT_ALAW:
-    ss->format = PA_SAMPLE_ALAW;
-    break;
-
-  case SF_FORMAT_FLOAT:
-  case SF_FORMAT_DOUBLE:
-  default:
-    ss->format = PA_SAMPLE_FLOAT32NE;
-    break;
-  }
-
-  ss->rate = (uint32_t) sfi.samplerate;
-  ss->channels = (uint8_t) sfi.channels;
-
-  if (!pa_sample_spec_valid(ss))
-    return -1;
-
-  return 0;
-}
-#define PA_ELEMENTSOF(x) (sizeof(x)/sizeof((x)[0]))
-int pa_sndfile_read_channel_map(SNDFILE *sf, pa_channel_map *cm)
-{
-
-  static const pa_channel_position_t table[] = {
-    [SF_CHANNEL_MAP_MONO] =                  PA_CHANNEL_POSITION_MONO,
-    [SF_CHANNEL_MAP_LEFT] =                  PA_CHANNEL_POSITION_FRONT_LEFT, /* libsndfile distinguishes left and front-left, which we don't */
-    [SF_CHANNEL_MAP_RIGHT] =                 PA_CHANNEL_POSITION_FRONT_RIGHT,
-    [SF_CHANNEL_MAP_CENTER] =                PA_CHANNEL_POSITION_FRONT_CENTER,
-    [SF_CHANNEL_MAP_FRONT_LEFT] =            PA_CHANNEL_POSITION_FRONT_LEFT,
-    [SF_CHANNEL_MAP_FRONT_RIGHT] =           PA_CHANNEL_POSITION_FRONT_RIGHT,
-    [SF_CHANNEL_MAP_FRONT_CENTER] =          PA_CHANNEL_POSITION_FRONT_CENTER,
-    [SF_CHANNEL_MAP_REAR_CENTER] =           PA_CHANNEL_POSITION_REAR_CENTER,
-    [SF_CHANNEL_MAP_REAR_LEFT] =             PA_CHANNEL_POSITION_REAR_LEFT,
-    [SF_CHANNEL_MAP_REAR_RIGHT] =            PA_CHANNEL_POSITION_REAR_RIGHT,
-    [SF_CHANNEL_MAP_LFE] =                   PA_CHANNEL_POSITION_LFE,
-    [SF_CHANNEL_MAP_FRONT_LEFT_OF_CENTER] =  PA_CHANNEL_POSITION_FRONT_LEFT_OF_CENTER,
-    [SF_CHANNEL_MAP_FRONT_RIGHT_OF_CENTER] = PA_CHANNEL_POSITION_FRONT_RIGHT_OF_CENTER,
-    [SF_CHANNEL_MAP_SIDE_LEFT] =             PA_CHANNEL_POSITION_SIDE_LEFT,
-    [SF_CHANNEL_MAP_SIDE_RIGHT] =            PA_CHANNEL_POSITION_SIDE_RIGHT,
-    [SF_CHANNEL_MAP_TOP_CENTER] =            PA_CHANNEL_POSITION_TOP_CENTER,
-    [SF_CHANNEL_MAP_TOP_FRONT_LEFT] =        PA_CHANNEL_POSITION_TOP_FRONT_LEFT,
-    [SF_CHANNEL_MAP_TOP_FRONT_RIGHT] =       PA_CHANNEL_POSITION_TOP_FRONT_RIGHT,
-    [SF_CHANNEL_MAP_TOP_FRONT_CENTER] =      PA_CHANNEL_POSITION_TOP_FRONT_CENTER,
-    [SF_CHANNEL_MAP_TOP_REAR_LEFT] =         PA_CHANNEL_POSITION_TOP_REAR_LEFT,
-    [SF_CHANNEL_MAP_TOP_REAR_RIGHT] =        PA_CHANNEL_POSITION_TOP_REAR_RIGHT,
-    [SF_CHANNEL_MAP_TOP_REAR_CENTER] =       PA_CHANNEL_POSITION_TOP_REAR_CENTER
-  };
-
-  SF_INFO sfi;
-  int sf_errno;
-  int *channels;
-  unsigned c;
-
-  // pa_assert(sf);
-  // pa_assert(cm);
-
-  // pa_zero(sfi);
-  if ((sf_errno = sf_command(sf, SFC_GET_CURRENT_SF_INFO, &sfi, sizeof(sfi)))) {
-    fprintf(stderr, "sndfile: %s", sf_error_number(sf_errno));
-    return -1;
-  }
-
-  channels = pa_xnew(int, sfi.channels);
-  if (!sf_command(sf, SFC_GET_CHANNEL_MAP_INFO, channels, sizeof(channels[0]) * sfi.channels)) {
-    pa_xfree(channels);
-    return -1;
-  }
-
-  cm->channels = (uint8_t) sfi.channels;
-  for (c = 0; c < cm->channels; c++) {
-    if (channels[c] <= SF_CHANNEL_MAP_INVALID ||
-        (unsigned) channels[c] >= PA_ELEMENTSOF(table)) {
-      pa_xfree(channels);
-      return -1;
-    }
-
-    cm->map[c] = table[channels[c]];
-  }
-
-  pa_xfree(channels);
-
-  if (!pa_channel_map_valid(cm))
-    return -1;
-
-  return 0;
-}
 
 - (void)playBytes:(NSNumber *)count
 {
@@ -238,30 +126,38 @@ int pa_sndfile_read_channel_map(SNDFILE *sf, pa_channel_map *cm)
   [stream playBuffer:buffer size:length tag:0];
 }
 
-- (void)playSystemBeep
+- (int)playSystemBeep
 {
   struct SF_INFO sfi;
   // char *file = "/usr/NextSpace/Sounds/Welcome-to-the-NeXT-world.snd";
   // char *file = "/usr/NextSpace/Sounds/SystemBeep.snd";
-  char    *file = "/usr/NextSpace/Sounds/Rooster.snd";
+  // char *file = "/usr/NextSpace/Sounds/Rooster.snd";
   // char *file = "/usr/share/sounds/alsa/Noise.wav";
   // char *file = "/usr/share/sounds/freedesktop/stereo/audio-channel-side-left.oga";
   // char *file = "/usr/share/sounds/KDE-Im-Irc-Event.ogg";
 
-  if (!(snd_file = sf_open(file, SFM_READ, &sfi))) {
-    fprintf(stderr, "Failed to open sound file.\n");
-    return;
+  if (file == NULL) {
+    file = "/usr/NextSpace/Sounds/Rooster.snd";
   }
 
-  if (pa_sndfile_read_sample_spec(snd_file, &sample_spec) < 0) {
+  if (!(snd_file = sf_open(file, SFM_READ, &sfi))) {
+    fprintf(stderr, "Failed to open sound file: unsupported format.\n");
+    return 1;
+  }
+
+  // Read sample specs
+  sample_spec.rate = (uint32_t)sfi.samplerate;
+  sample_spec.channels = (uint8_t)sfi.channels;
+  // We'll read bytes with `sf_readf_float()` in `-playBytes:`
+  sample_spec.format = PA_SAMPLE_FLOAT32LE;
+  if (!pa_sample_spec_valid(&sample_spec)) {
     fprintf(stderr, "Failed to determine sample specification from file.\n");
-    return;
+    return 1;
   }
   fprintf(stderr, "channels: %i rate: %i format: %i\n",
-          sample_spec.channels, sample_spec.rate, sample_spec.format);
-  sample_spec.format = PA_SAMPLE_FLOAT32LE;
+          sample_spec.channels, sample_spec.rate,  sample_spec.format);
   
-  // NSLog(@"Creating stream...");
+  NSLog(@"Creating stream...");
   stream = [[SNDPlayStream alloc] initOnDevice:[server defaultOutput]
                                   samplingRate:sample_spec.rate
                                   channelCount:sample_spec.channels
@@ -269,8 +165,10 @@ int pa_sndfile_read_channel_map(SNDFILE *sf, pa_channel_map *cm)
   [stream setDelegate:self];
   [stream setAction:@selector(playBytes:)];
   
-  // NSLog(@"Activate stream...");
+  NSLog(@"Activate stream...");
   [stream activate];
+
+  return 0;
 }
 
 @end
@@ -285,25 +183,34 @@ static void handle_signal(int sig)
 
 int main(int argc, char *argv[])
 {
-  NSAutoreleasePool *pool = [NSAutoreleasePool new];
-  NSConnection      *conn;
+  // NSAutoreleasePool *pool = [NSAutoreleasePool new];
+  // NSConnection      *conn;
 
-  client = [SoundKitClient new];
-  // conn = [NSConnection defaultConnection];
-  // [conn registerName:@"soundtool"];
+  if (argc < 2) {
+    fprintf(stderr, "Specify file absolute path name to sound file, please.\n");
+    return 1;
+  }
 
-  signal(SIGHUP, handle_signal);
-  signal(SIGINT, handle_signal);
-  signal(SIGQUIT, handle_signal);
-  signal(SIGTERM, handle_signal);
+  file = argv[1];
+
+  @autoreleasepool {
+    client = [SoundKitClient new];
+    // conn = [NSConnection defaultConnection];
+    // [conn registerName:@"soundtool"];
+
+    signal(SIGHUP, handle_signal);
+    signal(SIGINT, handle_signal);
+    signal(SIGQUIT, handle_signal);
+    signal(SIGTERM, handle_signal);
   
-  [client runLoopRun];
+    [client runLoopRun];
 
-  fprintf(stderr, "Runloop exited.\n");
+    fprintf(stderr, "Runloop exited.\n");
 
-  // [conn invalidate];
-  [client release];
-  [pool release];
+    // [conn invalidate];
+    [client release];
+    // [pool release];
+  }
 
   return 0;
 }
