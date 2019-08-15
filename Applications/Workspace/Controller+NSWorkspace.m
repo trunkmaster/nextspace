984be3b4a13f00a3f6afd21d7456ddaf976bb49b
Sergii Stoian <stoyan255@gmail.com>
2019-08-15 17:42:17 +0300

    Workspace: extract WorkspaceCenter into separate file Controller+WorkspaceCenter.

--- Applications/Workspace/Controller+NSWorkspace.m
+++ Applications/Workspace/Controller+NSWorkspace.m
@@ -47,6 +47,7 @@
 #import "Workspace+WM.h"
 
 #import "Controller+NSWorkspace.h"
+#import "Controller+WorkspaceCenter.h"
 
 #define PosixExecutePermission	(0111)
 
@@ -61,306 +62,6 @@ static NSImage	*folderImage = nil;
 static NSImage	*multipleFiles = nil;
 static NSImage	*unknownApplication = nil;
 static NSImage	*unknownTool = nil;
-
-//-----------------------------------------------------------------------------
-// Workspace notification center
-//-----------------------------------------------------------------------------
-
-static NSString	*GSWorkspaceNotification = @"GSWorkspaceNotification";
-static NSString *GSWorkspacePreferencesChanged = @"GSWorkspacePreferencesChanged";
-
-/*
- * Depending on the 'active' flag this returns either the currently
- * active application or an array containing all launched apps.<br />
- * The 'notification' argument is either nil (simply query on disk
- * database) or a notification containing information to be used to
- * update the database.
- */
-static id GSLaunched(NSNotification *notification, BOOL active)
-{
-  static NSString		*path = nil;
-  static NSDistributedLock	*lock = nil;
-  NSDictionary			*info = [notification userInfo];
-  NSString			*mode = [notification name];
-  NSMutableDictionary		*file = nil;
-  NSString			*name;
-  NSDictionary			*apps = nil;
-  BOOL				modified = NO;
-
-  NSLog(@"WorkspaceManager: GSLaunched() called");
-  
-  if (path == nil)
-    {
-      path = [NSTemporaryDirectory()
-	stringByAppendingPathComponent: @"GSLaunchedApplications"];
-      RETAIN(path);
-      lock = [[NSDistributedLock alloc] initWithPath:
-	[path stringByAppendingPathExtension: @"lock"]];
-    }
-  if ([lock tryLock] == NO)
-    {
-      unsigned	sleeps = 0;
-
-      /*
-       * If the lock is really old ... assume the app has died and break it.
-       */
-      if ([[lock lockDate] timeIntervalSinceNow] < -20.0)
-        {
-	  NS_DURING
-	    {
-	      [lock breakLock];
-	    }
-	  NS_HANDLER
-	    {
-              NSLog(@"Unable to break lock %@ ... %@", lock, localException);
-	    }
-	  NS_ENDHANDLER
-        }
-      /*
-       * Retry locking several times if necessary before giving up.
-       */
-      for (sleeps = 0; sleeps < 10; sleeps++)
-	{
-	  if ([lock tryLock] == YES)
-	    {
-	      break;
-	    }
-	  sleeps++;
-	  [NSThread sleepUntilDate: [NSDate dateWithTimeIntervalSinceNow: 0.1]];
-	}
-      if (sleeps >= 10)
-        {
-          NSLog(@"Unable to obtain lock %@", lock);
-          return nil;
-	}
-    }
-
-  if ([[NSFileManager defaultManager] isReadableFileAtPath: path] == YES)
-    {
-      file = [NSMutableDictionary dictionaryWithContentsOfFile: path];
-    }
-  if (file == nil)
-    {
-      file = [NSMutableDictionary dictionaryWithCapacity: 2];
-    }
-  apps = [file objectForKey: @"GSLaunched"];
-  if (apps == nil)
-    {
-      apps = [NSDictionary new];
-      [file setObject: apps forKey: @"GSLaunched"];
-      RELEASE(apps);
-    }
-
-  if (info != nil
-    && (name = [info objectForKey: @"NSApplicationName"]) != nil)
-    {
-      NSDictionary	*oldInfo = [apps objectForKey: name];
-
-      if ([mode isEqualToString:
-	NSApplicationDidResignActiveNotification] == YES
-	|| [mode isEqualToString:
-	  NSWorkspaceDidTerminateApplicationNotification] == YES)
-	{
-	  if ([name isEqual: [file objectForKey: @"GSActive"]] == YES)
-	    {
-	      [file removeObjectForKey: @"GSActive"];
-	      modified = YES;
-	    }
-	}
-      else if ([mode isEqualToString:
-	NSApplicationDidBecomeActiveNotification] == YES)
-	{
-	  if ([name isEqual: [file objectForKey: @"GSActive"]] == NO)
-	    {
-	      [file setObject: name forKey: @"GSActive"];
-	      modified = YES;
-	    }
-	}
-
-      if ([mode isEqualToString:
-	NSWorkspaceDidTerminateApplicationNotification] == YES)
-	{
-	  if (oldInfo != nil)
-	    {
-	      NSMutableDictionary	*m = [apps mutableCopy];
-
-	      [m removeObjectForKey: name];
-	      [file setObject: m forKey: @"GSLaunched"];
-	      apps = m;
-	      RELEASE(m);
-	      modified = YES;
-	    }
-	}
-      else if ([mode isEqualToString:
-	NSApplicationDidResignActiveNotification] == NO)
-	{
-	  if ([info isEqual: oldInfo] == NO)
-	    {
-	      NSMutableDictionary	*m = [apps mutableCopy];
-
-	      [m setObject: info forKey: name];
-	      [file setObject: m forKey: @"GSLaunched"];
-	      apps = m;
-	      RELEASE(m);
-	      modified = YES;
-	    }
-	}
-    }
-
-  if (modified == YES)
-    {
-      [file writeToFile: path atomically: YES];
-    }
-  [lock unlock];
-
-  if (active == YES)
-    {
-      NSString	*activeName = [file objectForKey: @"GSActive"];
-
-      if (activeName == nil)
-	{
-	  return nil;
-	}
-      return [apps objectForKey: activeName];
-    }
-  else
-    {
-      return [[file objectForKey: @"GSLaunched"] allValues];
-    }
-}
-
-@interface _WorkspaceCenter: NSNotificationCenter
-{
-  NSNotificationCenter	*remote;
-}
-- (void) _handleRemoteNotification: (NSNotification*)aNotification;
-- (void) _postLocal: (NSString*)name userInfo: (NSDictionary*)info;
-@end
-
-@implementation	_WorkspaceCenter
-
-- (void) dealloc
-{
-  [remote removeObserver: self name: nil object: GSWorkspaceNotification];
-  RELEASE(remote);
-  [super dealloc];
-}
-
-- (id) init
-{
-  self = [super init];
-  if (self != nil)
-    {
-      remote = RETAIN([NSDistributedNotificationCenter defaultCenter]);
-      NS_DURING
-	{
-	  [remote addObserver: self
-		     selector: @selector(_handleRemoteNotification:)
-			 name: nil
-		       object: GSWorkspaceNotification];
-	}
-      NS_HANDLER
-	{
-	  NSUserDefaults	*defs = [NSUserDefaults standardUserDefaults];
-
-	  if ([defs boolForKey: @"GSLogWorkspaceTimeout"])
-	    {
-	      NSLog(@"NSWorkspace caught exception %@: %@", 
-	        [localException name], [localException reason]);
-	    }
-	  else
-	    {
-	      [localException raise];
-	    }
-	}
-      NS_ENDHANDLER
-    }
-  return self;
-}
-
-/*
- * Post notification remotely - since we are listening for distributed
- * notifications, we will observe the notification arriving from the
- * distributed notification center, and it will get sent out locally too.
- */
-- (void) postNotification: (NSNotification*)aNotification
-{
-  NSNotification	*rem;
-  NSString		*name = [aNotification name];
-  NSDictionary		*info = [aNotification userInfo];
-
-  if ([name isEqual: NSWorkspaceDidTerminateApplicationNotification] == YES
-    || [name isEqual: NSWorkspaceDidLaunchApplicationNotification] == YES
-    || [name isEqualToString: NSApplicationDidBecomeActiveNotification] == YES
-    || [name isEqualToString: NSApplicationDidResignActiveNotification] == YES)
-    {
-      GSLaunched(aNotification, YES);
-    }
-
-  rem = [NSNotification notificationWithName: name
-				      object: GSWorkspaceNotification
-				    userInfo: info];
-  NS_DURING
-    {
-      [remote postNotification: rem];
-    }
-  NS_HANDLER
-    {
-      NSUserDefaults	*defs = [NSUserDefaults standardUserDefaults];
-
-      if ([defs boolForKey: @"GSLogWorkspaceTimeout"])
-	{
-	  NSLog(@"NSWorkspace caught exception %@: %@", 
-	    [localException name], [localException reason]);
-	}
-      else
-	{
-	  [localException raise];
-	}
-    }
-  NS_ENDHANDLER
-}
-
-- (void) postNotificationName: (NSString*)name 
-		       object: (id)object
-{
-  [self postNotification: [NSNotification notificationWithName: name
-							object: object]];
-}
-
-- (void) postNotificationName: (NSString*)name 
-		       object: (id)object
-		     userInfo: (NSDictionary*)info
-{
-  [self postNotification: [NSNotification notificationWithName: name
-							object: object
-						      userInfo: info]];
-}
-
-/*
- * Forward a notification from a remote application to observers in this
- * application.
- */
-- (void) _handleRemoteNotification: (NSNotification*)aNotification
-{
-  [self _postLocal: [aNotification name]
-	  userInfo: [aNotification userInfo]];
-}
-
-/*
- * Method allowing a notification to be posted locally.
- */
-- (void) _postLocal: (NSString*)name userInfo: (NSDictionary*)info
-{
-  NSNotification	*aNotification;
-
-  aNotification = [NSNotification notificationWithName: name
-						object: self
-					      userInfo: info];
-  [super postNotification: aNotification];
-}
-
-@end
 
 //-----------------------------------------------------------------------------
 // Workspace private methods
@@ -484,6 +185,7 @@ static NSString		*_rootPath = @"/";
     }
 }
 
+// NEXTSPACE
 - (id)initNSWorkspace
 {
   NSArray *documentDir;
@@ -511,7 +213,7 @@ static NSString		*_rootPath = @"/";
            name: @"GSHousekeeping"
          object: nil];
 
-  _workspaceCenter = [_WorkspaceCenter new];
+  _workspaceCenter = [WorkspaceCenter new];
   _iconMap = [NSMutableDictionary new];
   _launched = [NSMutableDictionary new];
   if (applications == nil)
@@ -1902,6 +1604,10 @@ inFileViewerRootedAtPath: (NSString*)rootFullpath
   if (appicon) {
     WWMDestroyLaunchingIcon(appicon);
   }
+  [_workspaceCenter
+    postNotificationName:NSWorkspaceDidTerminateApplicationNotification
+                  object:self
+                userInfo:@{@"NSApplicationName":[[task launchPath] lastPathComponent]}];
   
   if (exitCode != 0) {
     NXTRunAlertPanel(_(@"Workspace"),
