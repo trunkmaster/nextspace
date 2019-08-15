3fd74e96026bc57cbd3a77cd367ccc7527eaebb6
Sergii Stoian <stoyan255@gmail.com>
2019-08-16 02:01:47 +0300

    Password Preferences: include module into build/install process.

--- Applications/Preferences/Modules/Password/Password.m
+++ Applications/Preferences/Modules/Password/Password.m
@@ -19,34 +19,23 @@
 // Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
 //
 
-#import <math.h>
-
 #import <AppKit/NSApplication.h>
 #import <AppKit/NSNibLoading.h>
 #import <AppKit/NSView.h>
 #import <AppKit/NSBox.h>
 #import <AppKit/NSImage.h>
 #import <AppKit/NSButton.h>
-#import <AppKit/NSTableView.h>
-#import <AppKit/NSTableColumn.h>
-#import <AppKit/NSBrowser.h>
-#import <AppKit/NSMatrix.h>
 
 #import "Password.h"
 
 @implementation Password
 
-static NSBundle                 *bundle = nil;
-static NSUserDefaults           *defaults = nil;
-static NSMutableDictionary      *domain = nil;
+static NSBundle *bundle = nil;
 
 - (id)init
 {
   self = [super init];
   
-  defaults = [NSUserDefaults standardUserDefaults];
-  domain = [[defaults persistentDomainForName:NSGlobalDomain] mutableCopy];
-
   bundle = [NSBundle bundleForClass:[self class]];
   NSString *imagePath = [bundle pathForResource:@"Password" ofType:@"tiff"];
   image = [[NSImage alloc] initWithContentsOfFile:imagePath];
@@ -69,14 +58,12 @@ static NSMutableDictionary      *domain = nil;
 
 - (NSView *)view
 {
-  if (view == nil)
-    {
-      if (![NSBundle loadNibNamed:@"Password" owner:self])
-        {
-          NSLog (@"Password.preferences: Could not load NIB, aborting.");
-          return nil;
-        }
+  if (view == nil) {
+    if (![NSBundle loadNibNamed:@"Password" owner:self]) {
+      NSLog (@"Password.preferences: Could not load NIB, aborting.");
+      return nil;
     }
+  }
   
   return view;
 }
@@ -96,6 +83,9 @@ static NSMutableDictionary      *domain = nil;
 //
 - (IBAction)passwordChanged:(id)sender
 {
+  // Please type your old password.
+  // Please type your new password.
+  // Please type your new password.
 }
 
 @end
