1d55d80facdffc835020b3bc87f60da0d0894975
Sergii Stoian <stoyan255@gmail.com>
2020-07-21 11:33:17 +0300

    * System/etc/profile.d/nextspace.sh: cleanup.

--- System/etc/profile.d/nextspace.sh
+++ System/etc/profile.d/nextspace.sh
@@ -1,24 +1,33 @@
-# NextSpace additions to /etc/profile
+### NextSpace additions to /etc/profile
 
+#
+# Paths
+#
 NS_PATH="/usr/NextSpace/bin:/Library/bin:/usr/NextSpace/sbin:/Library/sbin"
 export PATH=$NS_PATH:$PATH
-
 export MANPATH=:/Library/Documentation/man:/usr/NextSpace/Documentation/man
+# Only user home lib dir here. Others in /etc/ld.so.conf.d/nextspace.conf
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/Library/Libraries"
+export GNUSTEP_PATHLIST="$HOME:/:/usr/NextSpace:/Network"
+export INFOPATH="$HOME/Library/Documentation/info:/Library/Documentation/info:/usr/NextSpace/Documentation/info"
 
+#
+# Localization
+#
 # Initialize time zone to system time zone, but only if not yet set.
-# NSGlobalDomain preference. Format 'Local Time Zone' = Europe/Kiev
 defaults read NSGlobalDomain "Local Time Zone" 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
     echo "Updating `Local Time Zone` preference..."
     TZ=`/usr/bin/env -i stat --printf "%N\n" /etc/localtime |sed "s,.*-> '.*/zoneinfo/\([^']*\)',\1,"`
     defaults write NSGlobalDomain "Local Time Zone" $TZ
 fi
+export LC_CTYPE=$LANG
 ### NSString encoding should be determined from system locale
 #export GNUSTEP_STRING_ENCODING="NSUTF8StringEncoding"
 ### LANGUAGES moved into NSLanguages in NSGlobalDomain preferences
 #export LANGUAGES="English"
 
-# If development package was installed set Gnustep developer variable
+# CentOS 7: if Clang from SCLo was installed - setup GNUstep developer environment
 if [ -d /Developer/Makefiles ];then
     export GNUSTEP_MAKEFILES="/Developer/Makefiles"
     if [ -f /opt/rh/llvm-toolset-7.0/enable ];then
@@ -26,16 +35,12 @@ if [ -d /Developer/Makefiles ];then
     fi
 fi
 
-# Only user home lib dir here. Others in /etc/ld.so.conf.d/nextspace.conf
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/Library/Libraries"
-export GNUSTEP_PATHLIST="$HOME:/:/usr/NextSpace:/Network"
-export INFOPATH="$HOME/Library/Documentation/info:/Library/Documentation/info:/usr/NextSpace/Documentation/info"
-
-export COLUMNS
-
+# Has been used in ~/,xinitrc - remove it?
 export NS_SYSTEM="/usr/NextSpace"
 
+#
 # Log file
+#
 export USER=`whoami`
 export UID=`id -u`
 GS_SECURE="/tmp/GNUstepSecure"$UID
@@ -44,8 +49,10 @@ if [ ! -d $GS_SECURE ];then
     mkdir $GS_SECURE
     chmod 700 $GS_SECURE
 fi
-        
-# Replace ~/.zshrc with NextSpace supplied version
+ 
+#
+# ZSH: replace ~/.zshrc with NextSpace supplied version
+#
 if [ $SHELL == "/bin/zsh" ]; then
     if [ -f ~/.zshrc.nextspace ]; then
         echo "Replacing .zshrc with .zshrc.nextspace"
