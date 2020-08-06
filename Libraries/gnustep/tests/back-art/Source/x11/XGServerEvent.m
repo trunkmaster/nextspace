7560a3b042ae0be8114786a789ce3608556b6cd4
Sergii Stoian <stoyan255@gmail.com>
2020-08-06 18:03:40 +0300

    * Libraries/gnustep/tests/back-art/Source/x11/XGServerEvent.m   (_handleTakeFocus:forContext:): if application (window) receives   WM_TAKE_FOCUS in hidden state it means window manager wants us to be   unhidden.   DESCRIPTION: There are 2 protocols defined in XGServerWindow.h:   WMFHideApplication and WMFHideOtherApplications. These protocols exist   to help GNUstep application and window manager notify each other   about "Hide" and "Hide Others" actions. There are no protocols for   "unhide" action.   TODO: pull this change to upstream gnustep-back project after testing.

--- Libraries/gnustep/tests/back-art/Source/x11/XGServerEvent.m
+++ Libraries/gnustep/tests/back-art/Source/x11/XGServerEvent.m
@@ -1949,10 +1949,11 @@ posixFileDescriptor: (NSPosixFileDescriptor*)fileDescriptor
      events */
   if ([NSApp isHidden])
     {
-      /* This often occurs when hidding an app, since a bunch of
-         windows get hidden at once, and the WM is searching for a
-         window to take focus after each one gets hidden. */
-      NSDebugLLog(@"Focus", @"WM take focus while hiding");
+      /* If window receives WM_TAKE_FOCUS and application is in hidden 
+         state - it's time to unhide. There's no other method to 
+         tell us to unhide. */
+      NSDebugLLog(@"Focus", @"WM take focus while in hidden state - unhiding.");
+      [NSApp unhide:nil];
     }
   else if (cWin->ignore_take_focus == YES)
     {
