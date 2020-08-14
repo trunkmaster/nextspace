46ec89ed8b9b3f6d956a5a50f0e0a341e05d02cb
Frederico Munoz <fsmunoz@gmail.com>
2020-08-14 16:35:40 +0100

    SELinux policies support: adds policies, changes to the spec and to the install script

--- Packaging/RedHat/nextspace_install.sh
+++ Packaging/RedHat/nextspace_install.sh
@@ -29,7 +29,7 @@ if [ $YN != "y" ]; then
     exit
 fi
 
-# Add EPEL package repository if needed
+# Add EPEL package repository
 if [ $EPEL_REPO != "" ]; then
     echo -n "Checking for EPEL repository installed..."
     yum repolist | grep "epel" 2>&1 > /dev/null
@@ -44,13 +44,6 @@ if [ $EPEL_REPO != "" ]; then
         echo -e -n "\e[0m"
     fi
 fi
-
-# More X drivers. Workaround until NextSpace RPMs include them as dependencies
-echo -n "Installing X11 drivers and utilities..."
-yum -y -q install xorg-x11-drivers xorg-x11-xinit xorg-x11-utils 2>&1 > /dev/null
-echo -e -n "\e[32m"
-echo "done"
-echo -e -n "\e[0m"
 
 # Hostname in /etc/hosts
 echo -n "Checking /etc/hosts..."
@@ -72,26 +65,48 @@ else
 fi
 
 # Disable SELinux
+
 echo -n "Checking SELinux configuration..."
-grep "SELINUX=enforcing" /etc/selinux/config 2>&1 > /dev/null
-if [ $? -eq 0 ];then
-    echo -e -n "\e[33m"
-    echo "configuring needed"
-    echo -e -n "\e[0m"
-    echo "Configuring SELinux (SELINUX=disabled)..."
-    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-    echo "+------------------------------------------------------------------------------+"
-    echo "| SELinux configuration has been changed. Linux needs reboot to apply changes. |"
-    echo "| Run this script after reboot to continue installation of NEXTSPACE.          |"
-    echo "+------------------------------------------------------------------------------+"
-    echo "  Press Enter to reboot or Crtl-C to cancel reboot and quit..."
-    read YN
-    shutdown -r now
+
+SELINUX_MODE=$(getenforce)
+echo ...done.
+echo
+echo -e -n "\e[1m"
+echo "Current SELinux mode is ${SELINUX_MODE}"
+echo
+echo -e -n "\e[0m"
+echo "Please choose the default SELinux mode"
+echo
+echo " 1) Permissive: SELinux will be active but will only log policy violations instead of enforcing them (default for NextSPACE)."
+echo " 2) Enforcing: SELinux will enforce the loaded policies and actively block access attempts which are not allowed (distro default)."
+echo " 3) Disabled: the SELinux subsystem will be disabled (choose this one if you have a strong reason for it)."
+echo
+echo "The recommended (and default) option is \"Permissive\", which will prevent SELinux from blocking accesses while logging them."
+echo "Choose \"Enforcing\" if you want or need to keep SELinux active, this will use the NextSTEP policies; keep in mind that they are a work in progress."
+echo "You can also choose to completely disable the SELinux subsystem; this should functionally be similar to \"Permissive\" but will not log anything."
+echo
+echo -e -n "\e[1m"
+echo -n "SELinux mode [default: 1]?"
+echo -e -n "\e[0m"
+read SEL
+echo -n "Setting SELinux default mode to "
+if [ 1$SEL -eq  12 ]; then
+    echo -n enforcing
+    sed -i -e ' s/SELINUX=.*/SELINUX=enforcing/' /etc/selinux/config
+    touch /.autorelabel
+elif [ 1$SEL -eq 13 ]; then
+    echo -n disabled
+    sed -i -e ' s/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
 else
-    echo -e -n "\e[32m"
-    echo "good"
-    echo -e -n "\e[0m"
+    echo -n permissive
+    sed -i -e ' s/SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
+    touch /.autorelabel
 fi
+echo "... done."
+echo "Filesystem with undergo automatic relabelling upon reboot for \"Permissive\" and \"Enforcing\" policies".
+echo
+echo -n "Installing NEXTSPACE SELinux policies..."
+
 
 # Install User packages
 echo -n "Installing NEXTSPACE User packages..."
@@ -100,6 +115,18 @@ ldconfig
 echo -e -n "\e[32m"
 echo "done"
 echo -e -n "\e[0m"
+
+echo "2. /usr/NextSpace" >> /tmp/l
+ls -ladZ /usr/NextSpace | tee -a /tmp/l
+ls -ladZ /usr/NextSpace/* | tee -a /tmp/l
+
+echo "2. /Library" >> /tmp/l
+ls -ladZ /Library | tee -a /tmp/l
+ls -ladZ /Library/* | tee -a /tmp/l
+
+echo "2. /tmp"
+ls -ladZ /GNU* | tee -a /tmp/l
+ls -ladZ /GNU/* | tee -a /tmp/l
 
 # Install Developer packages
 echo -e -n "\e[1m"
@@ -126,22 +153,30 @@ if [ $YN = "y" ]; then
     adduser -b /Users -s /bin/zsh -G audio,wheel $USERNAME
     echo "Setting up password..."
     passwd $USERNAME
+    echo "Updating SELinux file contexts..."
+    semodule -e ns-core
+    restorecon -R /Users
 fi
 
+echo "3. User"
+ls -laZ /Users/fsmunoz | tee -a /tmp/l
+ls -laZ /Users/fsmunoz/Library | tee -a /tmp/l
+
+
 # Setting up Login Panel
-#echo -e -n "\e[1m"
-#echo -n "Start graphical login panel on system boot? [yn]: "
-#echo -e -n "\e[0m"
-#read YN
-#if [ $YN = "y" ]; then
-#    systemctl set-default graphical.target
-#fi
+echo -e -n "\e[1m"
+echo -n "Start graphical login panel on system boot? [yn]: "
+echo -e -n "\e[0m"
+read YN
+if [ $YN = "y" ]; then
+    systemctl set-default graphical.target
+fi
 
 # Check if Login Panel works
-#echo -e -n "\e[1m"
-#echo -n "Do you want to start graphical login panel now? [yn]: "
-#echo -e -n "\e[0m"
-#read YN
-#if [ $YN = "y" ]; then
-#    systemctl start loginwindow
-#fi
+echo -e -n "\e[1m"
+echo -n "Do you want to start graphical login panel now? [yn]: "
+echo -e -n "\e[0m"
+read YN
+if [ $YN = "y" ]; then
+    systemctl start loginwindow
+fi
