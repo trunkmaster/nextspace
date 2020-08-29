2fce882acb484658bc753bc50c0777d6223b62c7
OnFlApp <onflapp@gmail.com>
2020-08-29 12:25:44 +0200

    initial commit

---
+++ Applications/Tools/nxpasteboard.h
@@ -0,0 +1,21 @@
+/* Include for nxworkspace tools
+   Copyright (C) 1998 Free Software Foundation, Inc.
+
+   Written by:  Richard Frith-Macdonald <richard@brainstorm.co.uk>
+   Created: October 1998
+
+   This file is part of the GNUstep Project
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   You should have received a copy of the GNU General Public
+   License along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+   */
+
+void printUsage();
---
+++ Applications/Tools/nxworkspace.h
@@ -0,0 +1,22 @@
+/* Include for nxworkspace tools
+   Copyright (C) 2020 Free Software Foundation, Inc.
+
+   Written by:  onflapp
+   Created: September 2020
+
+   This file is part of the NEXTSPACE Project
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   You should have received a copy of the GNU General Public
+   License along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+   */
+
+NSString* absolutePath(NSString* path);
+void printUsage();
