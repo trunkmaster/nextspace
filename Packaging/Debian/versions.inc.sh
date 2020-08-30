20121ee519d7dda81c45b893d8440b4042192546
Patrick Georgi <patrick@georgi.software>
2020-08-30 16:17:28 +0200

    Debian: update to latest master

--- Packaging/Debian/versions.inc.sh
+++ Packaging/Debian/versions.inc.sh
@@ -17,4 +17,5 @@ gnustep_back_version=0.28.0+nextspace
 gorm_version=1.2.26
 projectcenter_version=0.6.2
 
-nextspace_version=2b9968730c45805b7a7cb5d1b72f2e6cf57738a1
+nextspace_version=27f653c2abc9ef352e35ae1f654c85cbaef2b745
+
