e81193440cf9e53478fd873d4e8b8f077e54c8c8
Sergii Stoian <stoyan255@gmail.com>
2020-11-25 02:42:06 +0200

    Extend runloop test to X11 connection file descriptor handling.

--- Libraries/libCoreFoundation/tests/runloop_test.c
+++ Libraries/libCoreFoundation/tests/runloop_test.c
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-/* #include <sys/event.h> */
+#include <X11/Xlib.h>
 
 static void fdCallBack(CFFileDescriptorRef fdref, CFOptionFlags callBackTypes, void *info)
 {
@@ -20,23 +20,47 @@ static void fdCallBack(CFFileDescriptorRef fdref, CFOptionFlags callBackTypes, v
   close(fd);
 }
 
+static void timerCB(CFRunLoopTimerRef timer, void *data)
+{
+  fprintf(stderr, "Timer was fired: %s\n", (char *)data);
+}
+
 // one argument, an integer pid to watch, required
 int main(int argc, char *argv[])
 {
   CFFileDescriptorRef fdRef;
   CFRunLoopSourceRef rlSource;
+  int fd;
+  Display *dpy = NULL;
   
-  if (argc < 2) exit(1);
+  if (argc < 2) {
+    dpy = XOpenDisplay(getenv("DISPLAY"));
+    fd = ConnectionNumber(dpy);
+  }
+  else {  
+    fd = open(argv[1], O_RDONLY);
+  }
   
-  int fd = open(argv[1], O_RDONLY);
   fdRef = CFFileDescriptorCreate(kCFAllocatorDefault, fd, true, fdCallBack, NULL);
-  CFFileDescriptorEnableCallBacks(fdRef, kCFFileDescriptorWriteCallBack);
+  CFFileDescriptorEnableCallBacks(fdRef, kCFFileDescriptorReadCallBack);
   
   rlSource = CFFileDescriptorCreateRunLoopSource(kCFAllocatorDefault, fdRef, 0);
   CFRunLoopAddSource(CFRunLoopGetMain(), rlSource, kCFRunLoopDefaultMode);
   CFRelease(rlSource);
 
+  CFRunLoopTimerContext ctx = {0, "RunLoopTimer", NULL, NULL, 0};
+  CFRunLoopTimerRef timer =  CFRunLoopTimerCreate(kCFAllocatorDefault,
+                                                  CFAbsoluteTimeGetCurrent() + 3,
+                                                  0, 0, 0, timerCB, &ctx);
+  CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopDefaultMode);
+  CFRelease(timer);
+
   // run the run loop for 20 seconds
-  CFRunLoopRunInMode(kCFRunLoopDefaultMode, 20.0, false);
+  CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false);
+
+  if (dpy) {
+    XCloseDisplay(dpy);
+  }
+  
   return 0;
 }
