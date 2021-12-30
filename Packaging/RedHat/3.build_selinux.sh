fed1bd069f2a5e97e336b107bd0956f4b1214a07
armm77 <armm77@icloud.com>
2021-12-30 18:38:38 -0500

    The scripts for the automatic generation of the selinux package are added.

---
+++ Packaging/RedHat/3.build_selinux.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# This script uses `sudo` to install generated RPMs. Please make sure user you 
+# run this script as has appropriate rights.
+# Notice it always rebuilds and reinstalls packages even if they're already installed.
+#
+
+. `dirname $0`/functions
+
+if [ $# -eq 0 ];then
+    print_help
+    exit 1
+fi
+
+prepare_environment
+if [ $? -eq 1 ];then
+    print_ERR "Failed to setup building environment. Exiting..."
+    exit 1
+fi
+
+REPO_DIR=$1
+
+# NextSpace SELinux
+`dirname $0`/build_nextspace-selinux.sh $1
+if [ $? -eq 1 ]; then
+    echo "Aborting..."
+    exit 1
+fi
+
+print_OK " Build and install of NEXTSPACE SELinux SUCCEEDED!"
+
+exit 0
