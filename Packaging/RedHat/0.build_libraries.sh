trunkmaster/nextspace

commit c39d18871a620d2debe0324dbdd6011353decd0e

Update rpm generation script: - Removed support for CentOS 8 because it ended its life cycle. - Added a new font Saira Extra Condensed. - The information panels of gorm and projectcenter were updated. - Changed some icons of the gorm and projectcenter applications.

Packaging/RedHat/0.build_libraries.sh
@@ -17,16 +17,9 @@ if [ $? -eq 1 ];then
     exit 1
 fi
 
-# Build missed libraries for CentOS 8: libudisks2-devel and libart_lgpl
-if [ "$OS_NAME" == "CentOS Linux" ] && [ $OS_VERSION == "8" ];then
-    rpm -q libart_lgpl 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then
-        `dirname $0`/custom/build_libart.sh
-    fi
-    rpm -q libudisks-devel 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then
-        `dirname $0`/custom/build_udisks2.sh
-    fi
+if [ "$OS_NAME" == "Rocky Linux" ] || [ "$OS_NAME" == "AlmaLinux" ] && [ $OS_VERSION = "8.8" ];then
+    dnf -y install epel-release
+    dnf config-manager --set-enabled powertools
 fi
 
 REPO_DIR=$1
@@ -46,7 +39,7 @@ if [ $? -eq 1 ]; then
 fi
 
 # GNUstep Objective-C runtime
-if [ "$OS_NAME" == "CentOS Linux" ];then
+if [ "$OS_NAME" == "Rocky Linux" ] || [ "$OS_NAME" == "AlmaLinux" ];then
     `dirname $0`/build_libobjc2.sh $1
     if [ $? -eq 1 ]; then
         echo "Aborting..."

Applications/nextspace-applications.spec
@@ -115,7 +115,7 @@ export CMAKE=%{CMAKE}
 /usr/NextSpace
 
 %files devel
-/usr/NextSpace/include
+/usr/NextSpace/include/apps
 
 #
 # Package install

Core/nextspace-core.spec
@@ -108,8 +108,8 @@ mkdir -p %{buildroot}/usr/NextSpace/etc
 /root/.emacs.nextspace
 /root/.gtkrc-2.0
 /root/.zshrc.nextspace
-/etc/ld.so.conf.d
-/etc/profile.d
+/etc/ld.so.conf.d/nextspace.conf
+/etc/profile.d/nextspace.sh
 /etc/skel
 /etc/tuned
 /etc/udev
@@ -121,8 +121,9 @@ mkdir -p %{buildroot}/usr/NextSpace/etc
 /usr/NextSpace/bin/gnustep-services
 /usr/NextSpace/bin/openapp
 /usr/share/icons/NextSpace
-/usr/share/plymouth/themes
-/etc/dracut.conf.d
+/usr/share/plymouth/themes/nextspace
+/usr/share/plymouth/themes/plymouth-preview
+/etc/dracut.conf.d/vboxvideo.conf
 
 %files devel
 /Developer

Core/os_files/etc/dracut.conf.d/times_font.conf
@@ -1,1 +1,0 @@
-install_items+=" /usr/NextSpace/Frameworks/DesktopKit.framework/Resources/Fonts/Times.nfont "

Frameworks/DesktopKit/Resources/Fonts/Saira Extra Condensed.nfont/FontInfo.plist
@@ -1,0 +1,71 @@
+{
+    Faces = (
+        {
+            Files = (
+                SairaExtraCondensed-Regular.ttf
+            );
+            Name = "Regular";
+            PostScriptName = "SairaExtraCondensed Regular";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-Black.ttf
+            );
+            Name = "Black";
+            PostScriptName = "SairaExtraCondensed Black";
+        },
+        {
+            Files = (
+                SairaExtraCondensed-Bold.ttf
+            );
+            Name = "Bold";
+            PostScriptName = "SairaExtraCondensed Bold";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-ExtraBold.ttf
+            );
+            Name = "ExtraBold";
+            PostScriptName = "SairaExtraCondensed ExtraBold";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-ExtraLight.ttf
+            );
+            Name = "ExtraLight";
+            PostScriptName = "SairaExtraCondensed ExtraLight";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-Light.ttf
+            );
+            Name = "Light";
+            PostScriptName = "SairaExtraCondensed Light";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-Medium.ttf
+            );
+            Name = "Medium";
+            PostScriptName = "SairaExtraCondensed Medium";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-SemiBold.ttf
+            );
+            Name = "SemiBold";
+            PostScriptName = "SairaExtraCondensed Semi Bold";
+        },
+	{
+            Files = (
+                SairaExtraCondensed-Thin.ttf
+            );
+            Name = "Thin";
+            PostScriptName = "SairaExtraCondensed Thin";
+        }
+    );
+    Description = "Saira is a sans serif system. It features a huge range of weights and widths, ready for all kind of typographic challenges.";
+    FontCopyright = "Copyright 2016 The Saira Project Authors (omnibus.type@gmail.com), with reserved font name Saira.";
+    Packager = "Andres Morales <armm77@icloud.com>";
+    Version = 1.00;
+}

commit 50d597baf41b751b38c81c0bb2d0a85c31c780ba

- Fixed an error in the execution of the build_libobjc2.sh script introduced in the commit https://github.com/trunkmaster/nextspace/commit/c39d18871a620d2debe0324dbdd6011353decd0e

Packaging/RedHat/0.build_libraries.sh
@@ -39,12 +39,10 @@ if [ $? -eq 1 ]; then
 fi
 
 # GNUstep Objective-C runtime
-if [ "$OS_NAME" == "Rocky Linux" ] || [ "$OS_NAME" == "AlmaLinux" ];then
-    `dirname $0`/build_libobjc2.sh $1
-    if [ $? -eq 1 ]; then
-        echo "Aborting..."
-        exit 1
-    fi
+`dirname $0`/build_libobjc2.sh $1
+if [ $? -eq 1 ]; then
+    echo "Aborting..."
+    exit 1
 fi
 
 # NextSpace Core