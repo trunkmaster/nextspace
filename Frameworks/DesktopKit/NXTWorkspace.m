9cc787929ae1ea288fa5983a135f1c1e662ae379
Sergii Stoian <stoyan255@gmail.com>
2023-10-27 16:13:00 +0300

    add copyright to a new files.

--- Frameworks/DesktopKit/NXTWorkspace.m
+++ Frameworks/DesktopKit/NXTWorkspace.m
@@ -1,3 +1,24 @@
+/* -*- mode: objc -*- */
+//
+// Project: NEXTSPACE - DesktopKit framework
+//
+// Copyright (C) 2014-present Sergii Stoian
+//
+// This application is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This application is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this library; if not, write to the Free
+// Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+//
+
 #import "NXTWorkspace.h"
 
 // WM.plist
@@ -9,7 +30,7 @@ NSString* WMShouldHideOthersNotification = @"WMShouldHideOthersNotification";
 NSString* WMDidHideOthersNotification = @"WMDidHideOthersNotification";
 // Quit or Force Quit
 NSString* WMShouldTerminateApplicationNotification = @"WMShouldTerminateApplicationNotification";
-NSString * WMDidTerminateApplicationNotification = @"WMDidTerminateApplicationNotification";
+NSString* WMDidTerminateApplicationNotification = @"WMDidTerminateApplicationNotification";
 // Windows -> Zoom Window
 NSString* WMShouldZoomWindowNotification = @"WMShouldZoomWindowNotification";
 NSString* WMDidZoomWindowNotification = @"WMDidZoomWindowNotification";
