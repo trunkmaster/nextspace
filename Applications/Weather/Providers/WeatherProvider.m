2c49c85a77f08245932c17232ebee922d448e24c
Sergii Stoian <stoyan255@gmail.com>
2024-01-03 18:11:35 +0200

    Weather: add missed WeatherProvider dummy implementation.

---
+++ Applications/Weather/Providers/WeatherProvider.m
@@ -0,0 +1,31 @@
+#import <WeatherProvider.h>
+
+@implementation WeatherCurrent
+@end
+
+@implementation WeatherForecast
+@end
+
+@implementation WeatherProvider
+- (NSString *)name
+{
+  return @"WeatherProvider";
+}
+- (BOOL)setLocationByName:(NSString *)name
+{
+  return NO;
+}
+- (NSArray *)locationsListForName:(NSString *)name {
+  return nil;
+}
+- (NSDictionary *)temperatureUnitsList {
+  return nil;
+}
+- (void)setTemperatureUnits:(NSString *)name
+{
+}
+- (BOOL)fetchWeather
+{
+  return NO;
+}
+@end
