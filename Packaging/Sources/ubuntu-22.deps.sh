57130b6e2cd0a3c68666c0a30b57ee0ec4601b1f
Sergii Stoian <stoyan255@gmail.com>
2024-01-12 17:22:49 +0000

    Packaging/Sources: start adapting scripts to build on Ubuntu 22.04.

---
+++ Packaging/Sources/ubuntu-22.deps.sh
@@ -0,0 +1,25 @@
+# libdispatch
+clang
+cmake
+libtool
+libbsd-dev
+#libstdc++-dev - installed as dependency
+
+# libcorefoundation
+#libdispatch-dev
+libxml2-dev
+libicu-dev
+libcurl-dev
+libuuid-dev
+
+# libobjc2
+
+# gnustep-make
+
+# libwraster
+
+# gnustep-base
+
+# gnustep-gui
+
+# back-art
--- Packaging/Sources/versions.inc.sh
+++ Packaging/Sources/versions.inc.sh
@@ -1,12 +1,13 @@
-libdispatch_version=5.1.5
-libobjc2_version=2.0
-gnustep_make_version=2.7.0
-gnustep_base_version=1.27.0
-gnustep_gui_version=0.28.0+nextspace
-gnustep_back_version=0.28.0+nextspace
+libdispatch_version=5.4.2
+libcorefoundation_version=5.4.2
+libobjc2_version=2.1
+gnustep_make_version=2_9_1
+gnustep_base_version=1_29_0
+gnustep_gui_version=0_30_0
+#gnustep_back_version=0_30_0
 
-gorm_version=1.2.26
-projectcenter_version=0.6.2
+gorm_version=1_3_1
+projectcenter_version=0_7_0
 
 roboto_mono_version=0.2020.05.08
 roboto_mono_checkout=master
@@ -15,3 +16,21 @@ MAKE_CMD=make
 if type "gmake" 2>/dev/null >/dev/null ;then
   MAKE_CMD=gmake
 fi
+
+git_remote_archive() {
+  local url="$1"
+  local dest="$2-$3"
+  local branch="$4"
+
+  cd "$ROOT"
+
+  if [ -d "$dest" ];then
+    echo "$dest exists, skipping"
+  else
+	  git clone --recurse-submodules "$url" "$dest"
+    cd "$dest"
+    if [ "$branch" != "master" ];then
+      git checkout $branch
+    fi
+  fi
+}
