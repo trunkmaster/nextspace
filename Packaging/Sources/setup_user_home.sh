8410e585102ee10c5f0a704772e145c8e97b7fa7
Sergii Stoian <stoyan255@gmail.com>
2024-01-22 17:18:28 +0000

    copy .config to home directory without rewrite existing files.

--- Packaging/Sources/setup_user_home.sh
+++ Packaging/Sources/setup_user_home.sh
@@ -4,8 +4,10 @@ echo "Copying initial settings from $PWD/home"
 if [ -d "$HOME/Library" ];then
 	echo "$HOME/Library exists already, please make sure it is up to date"
 else
-	cp -R /etc/skel/Library  $HOME/
+	cp -R /etc/skel/Library $HOME/
 fi
+
+cp -Rn /etc/skel/.config $HOME/
 
 if [ -f "$HOME/.xinitrc" ];then
 	echo "$HOME/.xinitrc exists already, will not overwrite!"
