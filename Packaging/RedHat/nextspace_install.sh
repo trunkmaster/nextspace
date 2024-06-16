983137c1162113833785cfe2447fc0333df2e557
Sergii Stoian <stoyan255@gmail.com>
2024-06-16 03:16:34 +0300

    fine-tune install script for release on Fedora 39

--- Packaging/RedHat/nextspace_install.sh
+++ Packaging/RedHat/nextspace_install.sh
@@ -3,7 +3,8 @@
 # This script should be placed along with NSUser and NSDeveloper
 # directories.
 
-RELEASE=0.91
+RELEASE=0.95
+ENABLE_EPEL=""
 
 if [ -f /etc/os-release ]; then 
     source /etc/os-release
@@ -15,24 +16,31 @@ if [ -f /etc/os-release ]; then
         else
             EPEL_REPO=epel-release
         fi
+        ENABLE_EPEL="--enablerepo=epel"
     fi
 fi
 
+clear
 #===============================================================================
 # Main sequence
 #===============================================================================
 echo -e -n "\e[1m"
+echo "==============================================================================="
 echo "This script will install NEXTSPACE release $RELEASE and configure system."
-echo -n "Do you want to continue? [yn]: "
+echo "==============================================================================="
+echo -e -n "\e[33m"
+echo -n "Do you want to continue? [y/N]: "
 echo -e -n "\e[0m"
 read YN
-if [ $YN != "y" ]; then
+if [ "$YN" != "y" ]; then
     echo "OK, maybe next time. Exiting..."
     exit
 fi
 
+#===============================================================================
 # Add EPEL package repository if needed
-if [ $EPEL_REPO != "" ]; then
+#===============================================================================
+if [ "$EPEL_REPO" != "" ]; then
     echo -n "Checking for EPEL repository installed..."
     yum repolist | grep "epel" 2>&1 > /dev/null
     if [ $? -eq 1 ];then
@@ -47,14 +55,9 @@ if [ $EPEL_REPO != "" ]; then
     fi
 fi
 
-# More X drivers. Workaround until NextSpace RPMs include them as dependencies
-echo -n "Installing X11 drivers and utilities..."
-yum -y -q install xorg-x11-drivers xorg-x11-xinit xorg-x11-utils 2>&1 > /dev/null
-echo -e -n "\e[32m"
-echo "done"
-echo -e -n "\e[0m"
-
+#===============================================================================
 # Hostname in /etc/hosts
+#===============================================================================
 echo -n "Checking /etc/hosts..."
 HOSTNAME="`hostname -s`"
 grep "$HOSTNAME" /etc/hosts 2>&1 > /dev/null
@@ -66,89 +69,153 @@ if [ $? -eq 1 ];then
     echo "configuring needed"
     echo -e -n "\e[0m"
     echo "Configuring hostname ($HOSTNAME)..."
-    sed -i 's/localhost4.localdomain4/localhost4.localdomain4 '"$HOSTNAME"'/g' /etc/hosts
+    sudo sed -i 's/localhost4.localdomain4/localhost4.localdomain4 '"$HOSTNAME"'/g' /etc/hosts
 else
     echo -e -n "\e[32m"
-    echo "good"
+    echo -e "good"
     echo -e -n "\e[0m"
 fi
-
-# Disable SELinux
-
-echo -n "Checking SELinux configuration..."
-
-SELINUX_MODE=$(getenforce)
-echo ...done.
 echo
+
+#===============================================================================
+# Disable SELinux
+#===============================================================================
+#clear
+echo -e -n "\e[1m"
+echo "==============================================================================="
+echo "SELinux configuration"
+echo "==============================================================================="
+echo -e -n "\e[0m"
+#echo -n "Checking SELinux configuration..."
+SELINUX_MODE=$(getenforce)
+#echo -e -n "\e[32m"
+#echo -e "\b\b\b   ${SELINUX_MODE}"
+#echo -e -n "\e[0m"
 echo -e -n "\e[1m"
 echo "Current SELinux mode is ${SELINUX_MODE}"
-echo
 echo -e -n "\e[0m"
-echo "Please choose the default SELinux mode"
-echo
-echo " 1) Permissive: SELinux will be active but will only log policy violations instead of enforcing them (default for NEXTSPACE)."
-echo " 2) Enforcing: SELinux will enforce the loaded policies and actively block access attempts which are not allowed (distro default)."
-echo " 3) Disabled: the SELinux subsystem will be disabled (choose this one if you have a strong reason for it)."
-echo
-echo "The recommended (and default) option is \"Permissive\", which will prevent SELinux from blocking accesses while logging them."
-echo "Choose \"Enforcing\" if you want or need to keep SELinux active, this will use the NEXTSPACE policies; keep in mind that they are a work in progress."
-echo "You can also choose to completely disable the SELinux subsystem; this should functionally be similar to \"Permissive\" but will not log anything."
-echo
-echo -e -n "\e[1m"
-echo -n "SELinux mode [default: 1]?"
-echo -e -n "\e[0m"
-read SEL
-echo -n "Setting SELinux default mode to "
-if [ 1$SEL -eq  12 ]; then
-    echo -n enforcing
-    sed -i -e ' s/SELINUX=.*/SELINUX=enforcing/' /etc/selinux/config
-    touch /.autorelabel
-elif [ 1$SEL -eq 13 ]; then
-    echo -n disabled
-    sed -i -e ' s/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
-else
-    echo -n permissive
-    sed -i -e ' s/SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
-    touch /.autorelabel
-fi
-echo "... done."
-echo "Filesystem with undergo automatic relabelling upon reboot for \"Permissive\" and \"Enforcing\" policies".
-echo
-# TODO: echo -n "Installing NEXTSPACE SELinux policies..."
-
-
-# Install User packages
-echo -n "Installing NEXTSPACE User packages..."
-yum -y -q install --enablerepo=epel NSUser/*.rpm 2>&1 > /dev/null
-ldconfig
-echo -e -n "\e[32m"
-echo "done"
-echo -e -n "\e[0m"
-
-# Install Developer packages
-echo -e -n "\e[1m"
-echo -n "Do you want to install packages for NEXTSPACE development? [yn]: "
+echo -e -n "\e[33m"
+echo -n "Do you want to change your SELinux configuration? [y/N]: "
 echo -e -n "\e[0m"
 read YN
-if [ $YN = "y" ]; then
-    echo -n "Installing NEXTSPACE Developer packages..."
-    ENABLE_REPO="--enable-repo=epel"
-    if [ $OS_NAME == "centos" ] && [ $OS_VERSION == "7" ]; then
-        yum -y -q install centos-release-scl 2>&1 > /dev/null
-        ENABLE_REPO+=" --enable-repo=centos-sclo-sclo --enable-repo=centos-sclo-rh"
+if [ "$YN" = "y" ]; then
+    echo
+    echo "Please choose the default SELinux mode"
+    echo
+    echo " 1) Permissive: SELinux will be active but will only log policy violations"
+    echo "                instead of enforcing them (default for NEXTSPACE)."
+    echo " 2) Enforcing: SELinux will enforce the loaded policies and actively block"
+    echo "               access attempts which are not allowed (distro default)."
+    echo " 3) Disabled: the SELinux subsystem will be disabled (choose this one if you" 
+    echo "              have a strong reason for it)."
+    echo
+    echo "The recommended (and default) option is \"Permissive\", which will prevent "
+    echo "'SELinux from blocking accesses while logging them."
+    echo "Choose \"Enforcing\" if you want or need to keep SELinux active, this will "
+    echo "use the NEXTSPACE policies; keep in mind that they are a work in progress."
+    echo "You can also choose to completely disable the SELinux subsystem; this should "
+    echo "functionally be similar to \"Permissive\" but will not log anything."
+    echo
+    echo "Filesystem with undergo automatic relabelling upon reboot for \"Permissive\" "
+    echo "and \"Enforcing\" policies".
+    echo
+    echo -e -n "\e[1m"
+    echo -n "SELinux mode [default: 1]: "
+    echo -e -n "\e[0m"
+    read SEL
+    echo -n "Setting SELinux default mode to "
+    if [ "$SEL" = 2 ]; then
+        echo Enforcing...
+        sudo sed -i -e ' s/SELINUX=.*/SELINUX=enforcing/' /etc/selinux/config
+        sudo touch /.autorelabel
+    elif [ "$SEL" = 3 ]; then
+        echo Disabled...
+        sudo sed -i -e ' s/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+    else
+        echo Permissive...
+        sudo sed -i -e ' s/SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
+        sudo touch /.autorelabel
     fi
-    yum -y -q install ${ENABLE_REPO} NSDeveloper/*.rpm 2>&1 > /dev/null
+    echo "Done!"
+    # TODO: echo -n "Installing NEXTSPACE SELinux policies..."
+fi
+
+#===============================================================================
+# Install User packages
+#===============================================================================
+echo
+echo -e -n "\e[1m"
+echo "==============================================================================="
+echo "Installing NEXTSPACE User packages..."
+echo "==============================================================================="
+echo -e -n "\e[0m"
+echo -n "..."
+sudo yum -y -q install $ENABLE_EPEL NSUser/*.rpm 2>&1 > /dev/null || exit 1
+sudo ldconfig
+echo -e -n "\e[32m"
+echo -e "\b\b\bDone. User packages were installed."
+echo -e -n "\e[0m"
+echo
+
+#===============================================================================
+# Install Developer packages
+#===============================================================================
+echo -e -n "\e[33m"
+echo -n "Do you want to install packages for NEXTSPACE development? [yN]: "
+echo -e -n "\e[0m"
+read YN
+if [ "$YN" = "y" ]; then
+    echo -e -n "\e[1m"
+    echo "==============================================================================="
+    echo "Installing NEXTSPACE Developer packages..."
+    echo "==============================================================================="
+    echo -e -n "\e[0m"
+    echo -n "..."
+    if [ $OS_NAME == "centos" ]; then
+        if [ $VERSION_ID == "7" ]; then
+            sudo yum -y -q install centos-release-scl 2>&1 > /dev/null || exit 1
+            ENABLE_EPEL+=" --enable-repo=centos-sclo-sclo --enable-repo=centos-sclo-rh"
+        fi
+    fi
+    sudo yum -y -q install $ENABLE_EPEL NSDeveloper/*.rpm 2>&1 > /dev/null || exit 1
     echo -e -n "\e[32m"
-    echo "done"
+    echo -e "\b\b\bDone. Developer packages were installed."
     echo -e -n "\e[0m"
 fi
+echo
 
-# Adding user
+#===============================================================================
+# More X drivers. Workaround until NextSpace RPMs include them as dependencies
+#===============================================================================
 echo -e -n "\e[1m"
-echo -n "Do you want to add user? [yn]: "
+echo "==============================================================================="
+echo "Installing X11 drivers and utilities..."
+echo "==============================================================================="
+echo -e -n "\e[0m"
+echo -n "..."
+X11_DRIVERS="xorg-x11-drivers xorg-x11-xinit"
+if [ $OS_NAME == "centos" ]; then
+    X11_DRIVERS+=" xorg-x11-utils"
+fi
+sudo yum -y -q install $X11_DRIVERS 2>&1 > /dev/null || exit 1
+echo -e -n "\e[32m"
+echo -e "\b\b\bDone. X11 drivers and utilities were installed."
+echo -e -n "\e[0m"
+echo
+
+echo -e -n "\e[1m"
+echo "==============================================================================="
+echo "Post-install optional configuration"
+echo "==============================================================================="
+echo -e -n "\e[0m"
+#===============================================================================
+# Adding user
+#===============================================================================
+echo -e -n "\e[33m"
+echo -n "Do you want to add user? [y/N]: "
 echo -e -n "\e[0m"
 read YN
-if [ $YN = "y" ]; then
+if [ "$YN" = "y" ]; then
     echo -n "Please enter username: "
     read USERNAME
     echo "Adding username $USERNAME"
@@ -161,20 +228,28 @@ if [ $YN = "y" ]; then
     restorecon -R /Users 2>&1 > /dev/null
 fi
 
+#===============================================================================
 # Setting up Login Panel
-echo -e -n "\e[1m"
-echo -n "Start graphical login panel on system boot? [yn]: "
+#===============================================================================
+echo -e -n "\e[33m"
+echo -n "Start graphical login panel on system boot? [y/N]: "
 echo -e -n "\e[0m"
 read YN
-if [ $YN = "y" ]; then
+if [ "$YN" = "y" ]; then
     systemctl set-default graphical.target
 fi
 
+#===============================================================================
 # Check if Login Panel works
-echo -e -n "\e[1m"
-echo -n "Do you want to start graphical login panel now? [yn]: "
+#===============================================================================
+echo -e -n "\e[33m"
+echo -n "Do you want to start graphical login panel now? [y/N]: "
 echo -e -n "\e[0m"
 read YN
-if [ $YN = "y" ]; then
+if [ "$YN" = "y" ]; then
     systemctl start loginwindow
 fi
+
+echo -e "\e[32m"
+echo " NEXTSPACE $RELEASE successfuly installed! Wolcome to the NeXT world!"
+echo -e "\e[0m"
