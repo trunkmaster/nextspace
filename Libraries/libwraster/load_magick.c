e6cd2302d6b6efa29724c026e1e9ae86657dfa2a
Sergii Stoian <stoyan255@gmail.com>
2024-07-31 03:08:01 +0300

    libwraster: initial implementation of RLoadMagick with GraphicsMagick instead of ImageMagick.

--- Libraries/libwraster/load_magick.c
+++ Libraries/libwraster/load_magick.c
@@ -19,14 +19,20 @@
  *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
  *  MA 02110-1301, USA.
  */
-
-#ifdef USE_MAGICK
-#if USE_MAGICK < 7
+// #include <magick/api.h>
+#include <string.h>
 #include <wand/magick_wand.h>
-#else
-#include <MagickWand/MagickWand.h>
-#endif
-#endif
+// #include <wand/pixel_wand.h>
+// #include <magick/common.h>
+// #include <magick/constitute.h>
+
+// #ifdef USE_MAGICK
+// #if USE_MAGICK < 7
+// #include <wand/magick_wand.h>
+// #else
+// #include <MagickWand/MagickWand.h>
+// #endif
+// #endif
 
 #include "config.h"
 #include "wraster.h"
@@ -34,9 +40,10 @@
 #include "wr_i18n.h"
 
 
-static int RInitMagickIfNeeded(void);
+// static int RInitMagickIfNeeded(void);
 
 
+/*
 RImage *RLoadMagick(const char *file_name)
 {
 	RImage *image = NULL;
@@ -52,15 +59,16 @@ RImage *RLoadMagick(const char *file_name)
 		return NULL;
 	}
 
-	/* Create a wand */
+	// Create a wand
 	m_wand = NewMagickWand();
 
-	/* set the default background as transparent */
+	// set the default background as transparent
 	bg_wand = NewPixelWand();
 	PixelSetColor(bg_wand, "none");
 	MagickSetBackgroundColor(m_wand, bg_wand);
+	
 
-	/* Read the input image */
+	// Read the input image
 	if (!MagickReadImage(m_wand, file_name)) {
 		RErrorCode = RERR_BADIMAGEFILE;
 		goto bye;
@@ -91,39 +99,119 @@ RImage *RLoadMagick(const char *file_name)
 	}
 
 bye:
-	/* Tidy up */
+	// Tidy up
 	DestroyPixelWand(bg_wand);
 	MagickClearException(m_wand);
 	DestroyMagickWand(m_wand);
 
 	return image;
 }
+*/
+
+RImage *RLoadMagick(const char *file_name)
+{
+  RImage *image = NULL;
+  unsigned char *ptr;
+  unsigned long w, h;
+  MagickWand *magick_wand = NULL;
+  PixelWand *background = NULL;
+  MagickBool is_success;
+  MagickBool is_opaque;
+
+  //   if (RInitMagickIfNeeded()) {
+  //     RErrorCode = RERR_BADFORMAT;
+  //     return NULL;
+  //   }
+
+  fprintf(stderr, "RLoadMagick: %s\n", file_name);
+
+  InitializeMagick(NULL);
+
+  /* Create a wand */
+  magick_wand = NewMagickWand();
+
+  /* Read the input image */
+  if (MagickReadImage(magick_wand, file_name) != MagickPass) {
+    RErrorCode = RERR_BADIMAGEFILE;
+    fprintf(stderr, "failed to read image at path: %s\n", file_name);
+    goto bye;
+  }
+
+  /* set the default background as transparent */
+  background = NewPixelWand();
+  PixelSetColor(background, "none");
+  MagickSetImageBackgroundColor(magick_wand, background);
+  DestroyPixelWand(background);
+  
+  w = MagickGetImageWidth(magick_wand);
+  h = MagickGetImageHeight(magick_wand);
+
+  fprintf(stderr, "w: %lu, h: %lu\n", w, h);
+      
+  is_success = MagickIsOpaqueImage(magick_wand, &is_opaque);
+
+  image = RCreateImage(w, h, !is_opaque);
+  if (!image) {
+    RErrorCode = RERR_NOMEMORY;
+    goto bye;
+  }
+
+  fprintf(stderr, "RCreateImage()\n");
+
+  ptr = image->data;
+  if (is_opaque == MagickTrue) {
+    is_success =
+        MagickGetImagePixels(magick_wand, 0, 0, (size_t)w, (size_t)h, "RGB", CharPixel, ptr);
+    fprintf(stderr, "MagickGetImagePixels: %d, opaque\n", is_success);
+  } else {
+    is_success =
+        MagickGetImagePixels(magick_wand, 0, 0, (size_t)w, (size_t)h, "RGBA", CharPixel, ptr);
+    fprintf(stderr, "MagickGetImagePixels: %d, alpha\n", is_success);
+  }
+  
+  if (!is_success) {
+    RErrorCode = RERR_BADIMAGEFILE;
+    RReleaseImage(image);
+    image = NULL;
+    goto bye;
+  }
+
+  fprintf(stderr, "END\n");
+
+bye:
+  /* Tidy up */
+  DestroyMagickWand(magick_wand);
+  fprintf(stderr, "DestroyMagickWand\n");
+  // MagickClearException(magick_wand);
+
+  return image;
+}
 
 /* Track the state of the library in memory */
-static enum {
-	MW_NotReady,
-	MW_Ready
-} magick_state;
+// static enum {
+// 	MW_NotReady,
+// 	MW_Ready
+// } magick_state;
 
 /*
  * Initialise MagickWand, but only if it was not already done
  *
  * Return ok(0) when MagickWand is usable and fail(!0) if not usable
  */
-static int RInitMagickIfNeeded(void)
-{
-	if (magick_state == MW_NotReady) {
-		MagickWandGenesis();
-		magick_state = MW_Ready;
-	}
+// static int RInitMagickIfNeeded(void)
+// {
+//   if (magick_state == MW_NotReady) {
+//     MagickWandGenesis();
+//     magick_state = MW_Ready;
+//   }
 
-	return 0;
-}
+//   return 0;
+// }
 
 void RReleaseMagick(void)
 {
-	if (magick_state == MW_Ready) {
-		MagickWandTerminus();
-		magick_state = MW_NotReady;
-	}
+	// if (magick_state == MW_Ready) {
+	// 	MagickWandTerminus();
+	// 	magick_state = MW_NotReady;
+	// }
 }
