a9d53842daf06dce0264543ef38ea63cea9a4d86
Sergii Stoian <stoyan255@gmail.com>
2024-12-30 10:13:45 +0000

    remove unused applications

--- Applications/Network/NetworkManager/DBusIntrospectable.h
+++
@@ -1,7 +0,0 @@
-/* -*- mode: objc -*- */
-#import <Foundation/Foundation.h>
-
-// org.freedesktop.DBus.Introspectable
-@protocol DBusIntrospectable
-- (NSString*)Introspect;
-@end
--- Applications/Network/NetworkManager/DBusPeer.h
+++
@@ -1,8 +0,0 @@
-/* -*- mode: objc -*- */
-#import <Foundation/Foundation.h>
-
-// org.freedesktop.DBus.Peer
-@protocol DBusPeer
-- (NSString*)GetMachineId;
-- (void)Ping;
-@end
