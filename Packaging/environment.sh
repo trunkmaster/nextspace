e08bc7e53b88022fab9a3b5d874c3073cc13154b
Sergii Stoian <stoyan255@gmail.com>
2025-01-04 01:11:58 +0200

    extract functions from environement.sh into separate file; prepare RPM directories on first run of RPM build scripts.

--- Packaging/environment.sh
+++ Packaging/environment.sh
@@ -26,28 +26,6 @@ fi
 OS_NAME=$NAME
 ${ECHO} "OS:\t\t${OS_ID}-${OS_VERSION}"
 
-# Package dependencies for Debian/Ubuntu
-if [ ${OS_ID} = "debian" ] || [ ${OS_ID} = "ubuntu" ]; then
-    . ./${OS_ID}-${OS_VERSION}.deps.sh || exit 1
-fi
-
-#----------------------------------------
-# Library versions
-#----------------------------------------
-libdispatch_version=5.9.2
-libcorefoundation_version=5.9.2
-libcfnetwork_version=129.20
-libobjc2_version=2.2.1
-gnustep_make_version=2_9_2
-gnustep_base_version=1_30_0
-gnustep_gui_version=0_31_0
-
-gorm_version=1_4_0
-projectcenter_version=0_7_0
-
-roboto_mono_version=0.2020.05.08
-roboto_mono_checkout=master
-
 #----------------------------------------
 # Paths
 #----------------------------------------
@@ -71,6 +49,7 @@ if [ -z $BUILD_RPM ]; then
     DEST_DIR=""
   fi
 else
+  print_H2 "===== Create rpmbuild directories..."
   RELEASE_USR="$_PWD/$OS_ID-$OS_VERSION/NSUser"
   RELEASE_DEV="$_PWD/$OS_ID-$OS_VERSION/NSDeveloper"
   mkdir -p ${RELEASE_USR}
@@ -79,9 +58,38 @@ else
   RPM_SOURCES_DIR=~/rpmbuild/SOURCES
   RPM_SPECS_DIR=~/rpmbuild/SPECS
   RPMS_DIR=~/rpmbuild/RPMS/`uname -m`
+  mkdir -p $RPM_SOURCES_DIR
+  mkdir -p $RPM_SPECS_DIR
 
   ${ECHO} "RPMs directory:\t$RPMS_DIR"
 fi
+
+. ../functions.sh
+#----------------------------------------
+# Package dependencies
+#----------------------------------------
+if [ ${OS_ID} = "debian" ] || [ ${OS_ID} = "ubuntu" ]; then
+    . ./${OS_ID}-${OS_VERSION}.deps.sh || exit 1
+else
+    prepare_environment
+fi
+
+#----------------------------------------
+# Library versions
+#----------------------------------------
+libdispatch_version=5.9.2
+libcorefoundation_version=5.9.2
+libcfnetwork_version=129.20
+libobjc2_version=2.2.1
+gnustep_make_version=2_9_2
+gnustep_base_version=1_30_0
+gnustep_gui_version=0_31_0
+
+gorm_version=1_4_0
+projectcenter_version=0_7_0
+
+roboto_mono_version=0.2020.05.08
+roboto_mono_checkout=master
 
 #----------------------------------------
 # Tools
@@ -137,155 +145,3 @@ if [ "$OS_ID" = "fedora" ] || [ "$OS_LIKE" = "rhel" ] || [ "$OS_ID" = "debian" ]
   which clang++ 2>&1 > /dev/null || `echo "No clang++ compiler found. Please install clang++ package."; exit 1`
   CXX_COMPILER=`which clang++`
 fi
-
-###############################################################################
-# Functions
-###############################################################################
-
-git_remote_archive() {
-  local url="$1"
-  local dest="$2"
-  local branch="$3"
-
-  cd ${BUILD_ROOT}
-
-  if [ -d "$dest" ];then
-    echo "$dest exists, skipping"
-  else
-    git clone --recurse-submodules "$url" "$dest"
-    cd "$dest"
-    if [ "$branch" != "master" ];then
-      git checkout $branch
-    fi
-  fi
-}
-
-install_packages() {
-  apt-get install -y $@ || exit 1
-}
-
-uninstall_packages() {
-  apt-get purge -y $@ || exit 1
-}
-
-prepare_environment() 
-{
-    print_H1 " Prepare build environment"
-    print_H2 "===== Create rpmbuild directories..."
-    mkdir -p $RPM_SOURCES_DIR
-    mkdir -p $RPM_SPECS_DIR
-
-    print_H2 "===== Install RPM build tools..."
-    rpm -q rpm-build 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then BUILD_TOOLS+=" rpm-build"; fi
-    rpm -q rpmdevtools 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then BUILD_TOOLS+=" rpmdevtools"; fi
-    rpm -q make 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then BUILD_TOOLS+=" make"; fi
-    rpm -q patch 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then BUILD_TOOLS+=" patch"; fi
-
-    if [ -f /etc/os-release ]; then 
-	if [ "${OS_LIKE}" = "rhel" ] && [ "${OS_VERSION}" = "9" ];then
-            dnf -y install epel-release
-            dnf config-manager --set-enabled crb
-            dnf -y install clang
-        else
-	    if [ "$OS_ID" = "fedora" ];then
-                dnf -y install clang
-	    else
-		print_H2 ">>>>> Can't find /etc/os-release - this OS is unsupported."
-                return 1
-	    fi
-        fi
-    fi
-    
-    if [ "${BUILD_TOOLS}" != "" ]; then
-        sudo yum -y install ${BUILD_TOOLS}
-    fi
-}
-
-rpm_version()
-{
-    echo `rpmspec -q --qf "%{version}-%{release}.%{arch}:" $1 | awk -F: '{print $1}'`
-}
-
-# $1 - path to spec file
-build_rpm()
-{
-    SPEC_FILE=$1
-    spectool -g -R ${SPEC_FILE}
-    DEPS=`rpmspec -q --buildrequires ${SPEC_FILE} | awk -c '{print $1}'`
-    sudo yum -y install ${DEPS}
-    rpmbuild -bb ${SPEC_FILE}
-}
-
-# $1 - package name, $2 - rpm file path
-install_rpm()
-{
-    rpm -q $1 2>&1 > /dev/null
-    if [ $? -eq 1 ]; then 
-        INST_CMD=install
-    else
-        INST_CMD=reinstall
-    fi
-    sudo yum -y $INST_CMD $2
-}
-
-# Bold
-print_H1()
-{
-    echo -e -n "\e[1m"
-    echo "================================================================================"
-    echo -e -n "\e[1m"
-    echo -e "$1"
-    echo -e -n "\e[1m"
-    echo "================================================================================"
-    echo -e -n "\e[0m"
-}
-
-# Brown
-print_H2()
-{
-    echo -e -n "\e[33m"
-    echo -e "$1"
-    echo -e -n "\e[0m"
-}
-
-# Green
-print_OK()
-{
-    echo -e -n "\e[32m"
-    echo "================================================================================"
-    echo -e -n "\e[32m"
-    echo -e "$1"
-    echo -e -n "\e[32m"
-    echo "================================================================================"
-    echo -e -n "\e[0m"
-}
-
-# Red
-print_ERR()
-{
-    echo -e -n "\e[31m"
-    echo "================================================================================"
-    echo -e -n "\e[31m"
-    echo -e "$1"
-    echo -e -n "\e[31m"
-    echo "================================================================================"
-    echo -e -n "\e[0m"
-}
-
-print_help()
-{
-    SCRIPT_NAME=`basename $0`
-    print_ERR " ERROR: No NEXTSPACE directory specified."
-    printf "\n"
-    print_H2 "You have to specify directory where NEXTSPACE git clone resides."
-    print_H2 "For example, consider this scenario:"
-    printf "\n"
-    print_H2 "$ git clone https://github.com/trunkmaster/nextspace"
-    print_H2 "$ cd nextspace"
-    print_H2 "$ ./scripts/$SCRIPT_NAME ~/nextspace"
-    printf "\n"
-}
---
+++ Packaging/functions.sh
@@ -0,0 +1,148 @@
+###############################################################################
+# Functions
+###############################################################################
+
+git_remote_archive() {
+  local url="$1"
+  local dest="$2"
+  local branch="$3"
+
+  cd ${BUILD_ROOT}
+
+  if [ -d "$dest" ];then
+    echo "$dest exists, skipping"
+  else
+    git clone --recurse-submodules "$url" "$dest"
+    cd "$dest"
+    if [ "$branch" != "master" ];then
+      git checkout $branch
+    fi
+  fi
+}
+
+install_packages() {
+  apt-get install -y $@ || exit 1
+}
+
+uninstall_packages() {
+  apt-get purge -y $@ || exit 1
+}
+
+rpm_version()
+{
+    echo `rpmspec -q --qf "%{version}-%{release}.%{arch}:" $1 | awk -F: '{print $1}'`
+}
+
+# $1 - path to spec file
+build_rpm()
+{
+    SPEC_FILE=$1
+    spectool -g -R ${SPEC_FILE}
+    DEPS=`rpmspec -q --buildrequires ${SPEC_FILE} | awk -c '{print $1}'`
+    sudo yum -y install ${DEPS}
+    rpmbuild -bb ${SPEC_FILE}
+}
+
+# $1 - package name, $2 - rpm file path
+install_rpm()
+{
+    rpm -q $1 2>&1 > /dev/null
+    if [ $? -eq 1 ]; then 
+        INST_CMD=install
+    else
+        INST_CMD=reinstall
+    fi
+    sudo yum -y $INST_CMD $2
+}
+
+# Bold
+print_H1()
+{
+    echo -e -n "\e[1m"
+    echo "================================================================================"
+    echo -e -n "\e[1m"
+    echo -e "$1"
+    echo -e -n "\e[1m"
+    echo "================================================================================"
+    echo -e -n "\e[0m"
+}
+
+# Brown
+print_H2()
+{
+    echo -e -n "\e[33m"
+    echo -e "$1"
+    echo -e -n "\e[0m"
+}
+
+# Green
+print_OK()
+{
+    echo -e -n "\e[32m"
+    echo "================================================================================"
+    echo -e -n "\e[32m"
+    echo -e "$1"
+    echo -e -n "\e[32m"
+    echo "================================================================================"
+    echo -e -n "\e[0m"
+}
+
+# Red
+print_ERR()
+{
+    echo -e -n "\e[31m"
+    echo "================================================================================"
+    echo -e -n "\e[31m"
+    echo -e "$1"
+    echo -e -n "\e[31m"
+    echo "================================================================================"
+    echo -e -n "\e[0m"
+}
+
+print_help()
+{
+    SCRIPT_NAME=`basename $0`
+    print_ERR " ERROR: No NEXTSPACE directory specified."
+    printf "\n"
+    print_H2 "You have to specify directory where NEXTSPACE git clone resides."
+    print_H2 "For example, consider this scenario:"
+    printf "\n"
+    print_H2 "$ git clone https://github.com/trunkmaster/nextspace"
+    print_H2 "$ cd nextspace"
+    print_H2 "$ ./scripts/$SCRIPT_NAME ~/nextspace"
+    printf "\n"
+}
+
+prepare_environment() 
+{
+    print_H1 " Prepare build environment"
+    print_H2 "===== Install RPM build tools..."
+    
+    rpm -q rpm-build 2>&1 > /dev/null
+    if [ $? -eq 1 ]; then BUILD_TOOLS+=" rpm-build"; fi
+    rpm -q rpmdevtools 2>&1 > /dev/null
+    if [ $? -eq 1 ]; then BUILD_TOOLS+=" rpmdevtools"; fi
+    rpm -q make 2>&1 > /dev/null
+    if [ $? -eq 1 ]; then BUILD_TOOLS+=" make"; fi
+    rpm -q patch 2>&1 > /dev/null
+    if [ $? -eq 1 ]; then BUILD_TOOLS+=" patch"; fi
+
+    if [ -f /etc/os-release ]; then 
+	    if [ "${OS_ID}" = "rhel" ] && [ "${OS_VERSION}" = "9" ];then
+            sudo dnf -y install epel-release
+            sudo dnf config-manager --set-enabled crb
+            sudo dnf -y install clang
+        else
+            if [ "$OS_ID" = "fedora" ];then
+                sudo dnf -y install clang
+            else
+                print_H2 ">>>>> Can't find /etc/os-release - this OS is unsupported."
+                return 1
+            fi
+        fi
+    fi
+    
+    if [ "${BUILD_TOOLS}" != "" ]; then
+        sudo dnf -y install ${BUILD_TOOLS}
+    fi
+}
