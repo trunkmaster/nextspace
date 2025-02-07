cac8657be461683614f9daa3da29ccdcb1143eeb
Sergii Stoian <stoyan255@gmail.com>
2025-02-07 18:07:12 +0200

    gdb-Workspace.sh fixes

--- Applications/Workspace/gdb-Workspace.sh
+++ Applications/Workspace/gdb-Workspace.sh
@@ -3,8 +3,6 @@
 
 export CFNETWORK_LIBRARY_PATH="/usr/NextSpace/lib/libCFNetwork.so"
 
-gnustep-services start
-export DISPLAY=:0.0
 PROG_NAME="Workspace"
 PROG_PATH="/usr/NextSpace/Apps/Workspace.app/Workspace"
 PROG_ARGS="--GNU-Debug=DBus --GNU-Debug=dealloc"
@@ -12,10 +10,12 @@ PROG_ARGS="--GNU-Debug=DBus --GNU-Debug=dealloc"
 WS_PID="`ps auxw|grep ${PROG_NAME}|grep -v grep|grep -v /bin/sh|awk -c '{print $2}'`"
 if [ "$WS_PID" == "" ];then
     echo "Starting new '${PROG_NAME}' instance..."
+    export DISPLAY=:0.0
+    gnustep-services start
     gdb -q --args ${PROG_PATH} ${PROG_ARGS}
+    gnustep-services stop
 else
     echo "Found '${PROG_NAME}' PID: ${WS_PID}"
     gdb ${PROG_PATH} ${WS_PID}
 fi
 
-gnustep-services stop
