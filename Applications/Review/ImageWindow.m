c02c447872d8ed72cd0faf6932ad7b315ca8524c
Sergii Stoian <stoyan255@gmail.com>
2025-02-27 12:10:09 +0200

    Review: fixed compilation with new GNUstep libraries.

--- Applications/Review/ImageWindow.m
+++ Applications/Review/ImageWindow.m
@@ -1,4 +1,4 @@
-/* 
+/*
  * ImageWindow.m
  */
 
@@ -25,29 +25,28 @@
 - (void)tile
 {
   NSScroller *hScroller = [self horizontalScroller];
-  NSRect     hsFrame;
+  NSRect hsFrame;
 
   [super tile];
 
   hsFrame = [hScroller frame];
-  hsFrame.size.width -= [scaleBtn frame].size.width+2;
+  hsFrame.size.width -= [scaleBtn frame].size.width + 2;
   [hScroller setFrame:hsFrame];
 }
 
 - (void)drawRect:(NSRect)rect
 {
-  NSRect            hsFrame = [[self horizontalScroller] frame];
-  NSPoint           lineStart, lineEnd;
+  NSRect hsFrame = [[self horizontalScroller] frame];
+  NSPoint lineStart, lineEnd;
   NSGraphicsContext *ctxt = GSCurrentContext();
 
   [super drawRect:rect];
 
-  if (_rFlags.flipped_view)
-    {
-      lineStart.x = hsFrame.origin.x + hsFrame.size.width;
-      lineStart.y = hsFrame.origin.y - 1;
-      lineEnd.y = lineStart.y + hsFrame.size.height + 1;
-    }
+  if ([self isFlipped] != NO) {
+    lineStart.x = hsFrame.origin.x + hsFrame.size.width;
+    lineStart.y = hsFrame.origin.y - 1;
+    lineEnd.y = lineStart.y + hsFrame.size.height + 1;
+  }
 
   DPSsetgray(ctxt, 0.0);
   DPSsetlinewidth(ctxt, 1.0);
@@ -70,136 +69,128 @@
 
 - (id)initWithContentsOfFile:(NSString *)path
 {
-  NSAssert(path,@"No path specified!");
+  NSAssert(path, @"No path specified!");
 
-  if ((self = [super init]))
-    {
-      NSRect      frame = NSMakeRect(0,0,0,0);
-      RScrollView *scrollView = nil;
-      NSImageView *imageView = nil; 
-      NSImage     *image;
-      NSArray     *array;
-      int         wMask = (NSTitledWindowMask 
-                           | NSClosableWindowMask
-                           | NSMiniaturizableWindowMask 
-                           | NSResizableWindowMask);
+  if ((self = [super init])) {
+    NSRect frame = NSMakeRect(0, 0, 0, 0);
+    RScrollView *scrollView = nil;
+    NSImageView *imageView = nil;
+    NSImage *image;
+    NSArray *array;
+    int wMask = (NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask |
+                 NSResizableWindowMask);
 
-      attr = [[NSFileManager defaultManager] fileAttributesAtPath:path 
-	                                             traverseLink:NO];
-      RETAIN(attr);
+    attr = [[NSFileManager defaultManager] fileAttributesAtPath:path traverseLink:NO];
+    RETAIN(attr);
 
-      // Image loading
-      imagePath = [path copy];
-      if (!(image = [[NSImage alloc] initWithContentsOfFile:path]))
-	{
-	  NSRunAlertPanel(@"Open file", 
-			  @"File %@ doesn't contain image %@", 
-			  @"Dismiss", nil, nil, path, image);
-	  return nil;
-	}
-	
-      // Image
-      [image setBackgroundColor: [NSColor lightGrayColor]];
-      array = [image representations];
-      reps  = [array count];
-      rep   = [array objectAtIndex:0];
-
-      if (rep == nil)
-	{
-	  return nil;
-	}
-      [rep retain];
-
-      imageSize  = [image size];
-      frame.size = imageSize;
-
-      // ImageView and ScrollView
-      imageView  = [[NSImageView alloc] initWithFrame:frame];
-      [imageView setEditable:NO];
-      [imageView setImage:image];
-      RELEASE(image);
-
-      frame.size = [NSScrollView frameSizeForContentSize:[imageView frame].size
-	                           hasHorizontalScroller:YES
-		  	             hasVerticalScroller:YES
-				              borderType:NSNoBorder];
-      scrollView = [[RScrollView alloc] initWithFrame:frame];
-      [scrollView setHasVerticalScroller:YES];
-      [scrollView setHasHorizontalScroller:YES];
-      [scrollView setBorderType:NSNoBorder];
-      [scrollView setDocumentView:imageView];
-      RELEASE(imageView);
-      [scrollView setAutoresizingMask:(NSViewWidthSizable |
-				       NSViewHeightSizable)];
-      // Content view
-      box = [[NSBox alloc] init];
-      [box setTitlePosition:NSNoTitle];
-      [box setBorderType:NSNoBorder];
-      [box setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-      [box setContentViewMargins:NSMakeSize(0.0, 0.0)];
-      [box setFrameFromContentFrame:frame];
-
-      // Popup
-      scalePopup = [[NSPopUpButton alloc] 
-        initWithFrame:NSMakeRect([box frame].size.width-57, 0, 57, 17)];
-      [scalePopup setRefusesFirstResponder:YES];
-      [scalePopup addItemWithTitle:@"10%"];
-      [scalePopup addItemWithTitle:@"20%"];
-      [scalePopup addItemWithTitle:@"30%"];
-      [scalePopup addItemWithTitle:@"40%"];
-      [scalePopup addItemWithTitle:@"50%"];
-      [scalePopup addItemWithTitle:@"60%"];
-      [scalePopup addItemWithTitle:@"70%"];
-      [scalePopup addItemWithTitle:@"80%"];
-      [scalePopup addItemWithTitle:@"90%"];
-      [scalePopup addItemWithTitle:@"100%"];
-      [scalePopup addItemWithTitle:@"200%"];
-      [scalePopup addItemWithTitle:@"300%"];
-      [scalePopup addItemWithTitle:@"400%"];
-      [scalePopup addItemWithTitle:@"500%"];
-      [scalePopup addItemWithTitle:@"600%"];
-      [scalePopup addItemWithTitle:@"700%"];
-      [scalePopup setAutoresizingMask:(NSViewMaxYMargin | NSViewMinXMargin)];
-      [scalePopup selectItemWithTitle:@"100%"];
-      [scrollView setScaleView:scalePopup];
-      [scrollView tile];
-
-      [box addSubview:scrollView];
-      RELEASE(scrollView);
-      [box addSubview:scalePopup];
-      RELEASE(scalePopup);
-
-      // Window
-     frame = [NSWindow frameRectForContentRect:frame styleMask:wMask];
-      if (imageSize.width > ([[NSScreen mainScreen] frame].size.width-64))
-	{
-	  frame.size.width = [[NSScreen mainScreen] frame].size.width-164;
-	}
-      if (imageSize.height > ([[NSScreen mainScreen] frame].size.height-64))
-	 {
-	  frame.size.height = [[NSScreen mainScreen] frame].size.height-64;
-	 }
-      if (frame.size.width < 100) frame.size.width = 100;
-      if (frame.size.height < 100) frame.size.height = 100;
-
-      window = [[NSWindow alloc] initWithContentRect:frame
-	                                   styleMask:wMask
-					     backing:NSBackingStoreRetained
-					       defer:YES];
-      [window setReleasedWhenClosed:YES];
-      [window setDelegate:self];
-      [window setFrame:frame display:YES];
-      [window setMaxSize:frame.size];
-      [window setMinSize:NSMakeSize(100,100)];
-      [window setContentView:box];
-      RELEASE(box);
-      [window setTitleWithRepresentedFilename:path];
-      [window setReleasedWhenClosed:YES];
-
-      [window center];
-      [window makeKeyAndOrderFront:nil];
-      [window display];
+    // Image loading
+    imagePath = [path copy];
+    if (!(image = [[NSImage alloc] initWithContentsOfFile:path])) {
+      NSRunAlertPanel(@"Open file", @"File %@ doesn't contain image %@", @"Dismiss", nil, nil, path,
+                      image);
+      return nil;
     }
+
+    // Image
+    [image setBackgroundColor:[NSColor lightGrayColor]];
+    array = [image representations];
+    reps = [array count];
+    rep = [array objectAtIndex:0];
+
+    if (rep == nil) {
+      return nil;
+    }
+    [rep retain];
+
+    imageSize = [image size];
+    frame.size = imageSize;
+
+    // ImageView and ScrollView
+    imageView = [[NSImageView alloc] initWithFrame:frame];
+    [imageView setEditable:NO];
+    [imageView setImage:image];
+    RELEASE(image);
+
+    frame.size = [NSScrollView frameSizeForContentSize:[imageView frame].size
+                                 hasHorizontalScroller:YES
+                                   hasVerticalScroller:YES
+                                            borderType:NSNoBorder];
+    scrollView = [[RScrollView alloc] initWithFrame:frame];
+    [scrollView setHasVerticalScroller:YES];
+    [scrollView setHasHorizontalScroller:YES];
+    [scrollView setBorderType:NSNoBorder];
+    [scrollView setDocumentView:imageView];
+    RELEASE(imageView);
+    [scrollView setAutoresizingMask:(NSViewWidthSizable | NSViewHeightSizable)];
+    // Content view
+    box = [[NSBox alloc] init];
+    [box setTitlePosition:NSNoTitle];
+    [box setBorderType:NSNoBorder];
+    [box setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [box setContentViewMargins:NSMakeSize(0.0, 0.0)];
+    [box setFrameFromContentFrame:frame];
+
+    // Popup
+    scalePopup =
+        [[NSPopUpButton alloc] initWithFrame:NSMakeRect([box frame].size.width - 57, 0, 57, 17)];
+    [scalePopup setRefusesFirstResponder:YES];
+    [scalePopup addItemWithTitle:@"10%"];
+    [scalePopup addItemWithTitle:@"20%"];
+    [scalePopup addItemWithTitle:@"30%"];
+    [scalePopup addItemWithTitle:@"40%"];
+    [scalePopup addItemWithTitle:@"50%"];
+    [scalePopup addItemWithTitle:@"60%"];
+    [scalePopup addItemWithTitle:@"70%"];
+    [scalePopup addItemWithTitle:@"80%"];
+    [scalePopup addItemWithTitle:@"90%"];
+    [scalePopup addItemWithTitle:@"100%"];
+    [scalePopup addItemWithTitle:@"200%"];
+    [scalePopup addItemWithTitle:@"300%"];
+    [scalePopup addItemWithTitle:@"400%"];
+    [scalePopup addItemWithTitle:@"500%"];
+    [scalePopup addItemWithTitle:@"600%"];
+    [scalePopup addItemWithTitle:@"700%"];
+    [scalePopup setAutoresizingMask:(NSViewMaxYMargin | NSViewMinXMargin)];
+    [scalePopup selectItemWithTitle:@"100%"];
+    [scrollView setScaleView:scalePopup];
+    [scrollView tile];
+
+    [box addSubview:scrollView];
+    RELEASE(scrollView);
+    [box addSubview:scalePopup];
+    RELEASE(scalePopup);
+
+    // Window
+    frame = [NSWindow frameRectForContentRect:frame styleMask:wMask];
+    if (imageSize.width > ([[NSScreen mainScreen] frame].size.width - 64)) {
+      frame.size.width = [[NSScreen mainScreen] frame].size.width - 164;
+    }
+    if (imageSize.height > ([[NSScreen mainScreen] frame].size.height - 64)) {
+      frame.size.height = [[NSScreen mainScreen] frame].size.height - 64;
+    }
+    if (frame.size.width < 100)
+      frame.size.width = 100;
+    if (frame.size.height < 100)
+      frame.size.height = 100;
+
+    window = [[NSWindow alloc] initWithContentRect:frame
+                                         styleMask:wMask
+                                           backing:NSBackingStoreRetained
+                                             defer:YES];
+    [window setReleasedWhenClosed:YES];
+    [window setDelegate:self];
+    [window setFrame:frame display:YES];
+    [window setMaxSize:frame.size];
+    [window setMinSize:NSMakeSize(100, 100)];
+    [window setContentView:box];
+    RELEASE(box);
+    [window setTitleWithRepresentedFilename:path];
+    [window setReleasedWhenClosed:YES];
+
+    [window center];
+    [window makeKeyAndOrderFront:nil];
+    [window display];
+  }
 
   return self;
 }
@@ -226,24 +217,20 @@
 
 - (void)windowWillClose:(NSNotification *)notif
 {
-  if ([[notif object] isEqual:window])
-	{
-	  if (delegate && 
-		  [delegate respondsToSelector:@selector(imageWindowWillClose:)])
-		{
-		  [delegate imageWindowWillClose:self];
-		  [window setDelegate: nil];
-		  window = nil;
-	  }
-	}
+  if ([[notif object] isEqual:window]) {
+    if (delegate && [delegate respondsToSelector:@selector(imageWindowWillClose:)]) {
+      [delegate imageWindowWillClose:self];
+      [window setDelegate:nil];
+      window = nil;
+    }
+  }
 }
 
 - (void)windowDidBecomeKey:(NSNotification *)aNotification
 {
-  if( [[aNotification object] isEqual:window] )
-	{
-	  [[Inspector sharedInspector] imageWindowDidBecomeActive:self];
-	}
+  if ([[aNotification object] isEqual:window]) {
+    [[Inspector sharedInspector] imageWindowDidBecomeActive:self];
+  }
 }
 
 - (NSString *)path
@@ -270,7 +257,7 @@
 {
   int bytes = [[attr objectForKey:@"NSFileSize"] intValue];
 
-  return [NSString stringWithFormat:@"%d Bytes",bytes];
+  return [NSString stringWithFormat:@"%d Bytes", bytes];
 }
 
 - (NSString *)imageFileModificationDate
@@ -296,12 +283,12 @@
 
 - (NSString *)imageResolution
 {
-  return [NSString stringWithFormat:@"%.1f x %.1f",imageSize.width,imageSize.height];
+  return [NSString stringWithFormat:@"%.1f x %.1f", imageSize.width, imageSize.height];
 }
 
 - (NSString *)bitsPerSample
 {
-  return [NSString stringWithFormat:@"%ld",[rep bitsPerSample]];
+  return [NSString stringWithFormat:@"%ld", [rep bitsPerSample]];
 }
 
 - (NSString *)colorSpaceName
@@ -311,12 +298,12 @@
 
 - (NSString *)hasAlpha
 {
-  return [NSString stringWithFormat:@"%@",[rep hasAlpha]?@"Yes":@"No"];
+  return [NSString stringWithFormat:@"%@", [rep hasAlpha] ? @"Yes" : @"No"];
 }
 
 - (NSString *)imageReps
 {
-  return [NSString stringWithFormat:@"%d",reps];
+  return [NSString stringWithFormat:@"%d", reps];
 }
 
 @end
--- Applications/Review/PrefController.m
+++ Applications/Review/PrefController.m
@@ -1,4 +1,4 @@
-/* 
+/*
  * PrefController.m created by probert on 2001-11-15 20:24:08 +0000
  *
  * Project ImageViewer
@@ -15,187 +15,187 @@
 
 - (id)init
 {
-    if ( self = [super init] ) {
-        unsigned int style = NSTitledWindowMask | NSClosableWindowMask;
-        NSRect winFrame = NSMakeRect(200,300,260,280);
-	NSRect rect;
-        NSTextField *textField;
-	NSBox *box;
-        NSMatrix *matrix;
-        NSButton *button;
-	NSButtonCell* buttonCell = [[NSButtonCell alloc] init];
-	NSDictionary *prefs;
+  if (self = [super init]) {
+    unsigned int style = NSTitledWindowMask | NSClosableWindowMask;
+    NSRect winFrame = NSMakeRect(200, 300, 260, 280);
+    NSRect rect;
+    NSTextField *textField;
+    NSBox *box;
+    NSMatrix *matrix;
+    NSButton *button;
+    NSButtonCell *buttonCell = [[NSButtonCell alloc] init];
+    NSDictionary *prefs;
 
-        prefs =[[NSUserDefaults standardUserDefaults] dictionaryRepresentation];
-        prefDict = [[NSMutableDictionary alloc] initWithDictionary:prefs];
+    prefs = [[NSUserDefaults standardUserDefaults] dictionaryRepresentation];
+    prefDict = [[NSMutableDictionary alloc] initWithDictionary:prefs];
 
-        preferences = [[NSWindow alloc] initWithContentRect:winFrame
-                                                styleMask:style
-                                                  backing:NSBackingStoreBuffered
-                                                    defer:YES];
-        [preferences setMinSize:NSMakeSize(260,280)];
-        [preferences setTitle:@"Preferences"];
-        [preferences setDelegate:self];
-        [preferences setReleasedWhenClosed:NO];
-        [preferences center];
-        [preferences setFrameAutosaveName:@"Preferences"];
+    preferences = [[NSWindow alloc] initWithContentRect:winFrame
+                                              styleMask:style
+                                                backing:NSBackingStoreBuffered
+                                                  defer:YES];
+    [preferences setMinSize:NSMakeSize(260, 280)];
+    [preferences setTitle:@"Preferences"];
+    [preferences setDelegate:self];
+    [preferences setReleasedWhenClosed:NO];
+    [preferences center];
+    [preferences setFrameAutosaveName:@"Preferences"];
 
-        box = [[NSBox alloc] init];
-	[box setTitle:@"Image Cache"];
-	[box setFrameFromContentFrame:NSMakeRect(16,180,228,64)];
-	[[preferences contentView] addSubview:box];
-	RELEASE(box);
+    box = [[NSBox alloc] init];
+    [box setTitle:@"Image Cache"];
+    [box setFrameFromContentFrame:NSMakeRect(16, 180, 228, 64)];
+    [[preferences contentView] addSubview:box];
+    RELEASE(box);
 
-        textField = [[NSTextField alloc] initWithFrame:NSMakeRect(8,16,80,21)];
-        [textField setAlignment: NSRightTextAlignment];
-        [textField setBordered: NO];
-        [textField setEditable: NO];
-        [textField setBezeled: NO];
-        [textField setDrawsBackground: NO];
-        [textField setStringValue:@"Cache size:"];
-        [box addSubview:textField];
-        RELEASE(textField);
+    textField = [[NSTextField alloc] initWithFrame:NSMakeRect(8, 16, 80, 21)];
+    [textField setAlignment:NSRightTextAlignment];
+    [textField setBordered:NO];
+    [textField setEditable:NO];
+    [textField setBezeled:NO];
+    [textField setDrawsBackground:NO];
+    [textField setStringValue:@"Cache size:"];
+    [box addSubview:textField];
+    RELEASE(textField);
 
-        rect = NSMakeRect(92,16,124,21);
-        cacheSizeField = [[NSTextField alloc] initWithFrame:rect];
-        [cacheSizeField setAlignment: NSRightTextAlignment];
-        [cacheSizeField setBordered: NO];
-        [cacheSizeField setEditable: YES];
-        [cacheSizeField setBezeled: YES];
-        [cacheSizeField setDrawsBackground: YES];
-        [cacheSizeField setTarget:self];
-        [cacheSizeField setAction:@selector(setCacheSize:)];
-	[box addSubview:cacheSizeField];
-	RELEASE(cacheSizeField);
+    rect = NSMakeRect(92, 16, 124, 21);
+    cacheSizeField = [[NSTextField alloc] initWithFrame:rect];
+    [cacheSizeField setAlignment:NSRightTextAlignment];
+    [cacheSizeField setBordered:NO];
+    [cacheSizeField setEditable:YES];
+    [cacheSizeField setBezeled:YES];
+    [cacheSizeField setDrawsBackground:YES];
+    [cacheSizeField setTarget:self];
+    [cacheSizeField setAction:@selector(setCacheSize:)];
+    [box addSubview:cacheSizeField];
+    RELEASE(cacheSizeField);
 
-        rect = NSMakeRect(32,144,220,15);
-        openRecursive =[[NSButton alloc] initWithFrame:rect];
-        [openRecursive setTitle:@"Open path recursively"];
-        [openRecursive setButtonType:NSSwitchButton];
-        [openRecursive setBordered:NO];
-        [openRecursive setTarget:self];
-        [openRecursive setAction:@selector(setOpenRecursive:)];
-        [openRecursive setContinuous:NO];
-        [[preferences contentView] addSubview:openRecursive];
-        [openRecursive sizeToFit];
-	RELEASE(openRecursive);
+    rect = NSMakeRect(32, 144, 220, 15);
+    openRecursive = [[NSButton alloc] initWithFrame:rect];
+    [openRecursive setTitle:@"Open path recursively"];
+    [openRecursive setButtonType:NSSwitchButton];
+    [openRecursive setBordered:NO];
+    [openRecursive setTarget:self];
+    [openRecursive setAction:@selector(setOpenRecursive:)];
+    [openRecursive setContinuous:NO];
+    [[preferences contentView] addSubview:openRecursive];
+    [openRecursive sizeToFit];
+    RELEASE(openRecursive);
 
-        rect = NSMakeRect(132,8,116,24);
-        matrix = [[NSMatrix alloc] initWithFrame: rect
-                                            mode: NSHighlightModeMatrix
-                                       prototype: buttonCell
-                                    numberOfRows: 1
-                                 numberOfColumns: 2];
-        [matrix setSelectionByRect:YES];
-        [matrix setAutoresizingMask: (NSViewMinXMargin | NSViewMaxYMargin)];
-        [matrix setTarget:self];
-        [matrix setAction:@selector(buttonsPressed:)];
-        [matrix setIntercellSpacing: NSMakeSize(2,2)];
-        [[preferences contentView] addSubview:matrix];
-        RELEASE(matrix);
-        RELEASE(buttonCell);
+    rect = NSMakeRect(132, 8, 116, 24);
+    matrix = [[NSMatrix alloc] initWithFrame:rect
+                                        mode:NSHighlightModeMatrix
+                                   prototype:buttonCell
+                                numberOfRows:1
+                             numberOfColumns:2];
+    [matrix setSelectionByRect:YES];
+    [matrix setAutoresizingMask:(NSViewMinXMargin | NSViewMaxYMargin)];
+    [matrix setTarget:self];
+    [matrix setAction:@selector(buttonsPressed:)];
+    [matrix setIntercellSpacing:NSMakeSize(2, 2)];
+    [[preferences contentView] addSubview:matrix];
+    RELEASE(matrix);
+    RELEASE(buttonCell);
 
-        button = [matrix cellAtRow:0 column:0];
-        [button setTag:0];
-        [button setStringValue:@"Reset"];
-        [button setBordered:YES];
-        [button setButtonType:NSMomentaryPushButton];
+    button = [matrix cellAtRow:0 column:0];
+    [button setTag:0];
+    [button setStringValue:@"Reset"];
+    [button setBordered:YES];
+    [button setButtonType:NSMomentaryPushButton];
 
-        button = [matrix cellAtRow:0 column:1];
-        [button setTag:1];
-        [button setStringValue:@"Set"];
-        [button setBordered:YES];
-        [button setButtonType:NSMomentaryPushButton];
-    }
-    return self;
+    button = [matrix cellAtRow:0 column:1];
+    [button setTag:1];
+    [button setStringValue:@"Set"];
+    [button setBordered:YES];
+    [button setButtonType:NSMomentaryPushButton];
+  }
+  return self;
 }
 
 - (void)dealloc
 {
-    RELEASE(preferences);
-    RELEASE(prefDict);
+  RELEASE(preferences);
+  RELEASE(prefDict);
 
-    [super dealloc];
+  [super dealloc];
 }
 
 - (void)show
 {
-    NSString *string = nil;
+  NSString *string = nil;
 
-    if (![preferences isVisible]) { 
-        [preferences setFrameUsingName:@"Preferences"];
-    }
+  if (![preferences isVisible]) {
+    [preferences setFrameUsingName:@"Preferences"];
+  }
 
-    string = [prefDict objectForKey:@"CacheSize"];
-    [cacheSizeField setStringValue:(string) ? string : @"50"];
+  string = [prefDict objectForKey:@"CacheSize"];
+  [cacheSizeField setStringValue:(string) ? string : @"50"];
 
-    string = [prefDict objectForKey:@"OpenRec"];
-    [openRecursive setState:([string isEqualToString:@"YES"]) ? 
-                                                    NSOnState : NSOffState];
+  string = [prefDict objectForKey:@"OpenRec"];
+  [openRecursive setState:([string isEqualToString:@"YES"]) ? NSOnState : NSOffState];
 
-    [preferences makeKeyAndOrderFront:self];
+  [preferences makeKeyAndOrderFront:self];
 }
 
 - (void)setCacheSize:(id)sender
 {
-    NSString *val = [cacheSizeField stringValue];
+  NSString *val = [cacheSizeField stringValue];
 
-    [prefDict setObject:val forKey:@"CacheSize"];
-    [preferences setDocumentEdited:YES];
+  [prefDict setObject:val forKey:@"CacheSize"];
+  [preferences setDocumentEdited:YES];
 }
 
 - (void)setOpenRecursive:(id)sender
 {
-    switch ([[sender selectedCell] state]) {
-        case 0:
-            [prefDict setObject:@"NO" forKey:@"OpenRec"];
-            break;
-        case 1:
-            [prefDict setObject:@"YES" forKey:@"OpenRec"];
-            break;
-    }
-    
-    [preferences setDocumentEdited:YES];
+  switch ([[sender selectedCell] state]) {
+    case 0:
+      [prefDict setObject:@"NO" forKey:@"OpenRec"];
+      break;
+    case 1:
+      [prefDict setObject:@"YES" forKey:@"OpenRec"];
+      break;
+    default:
+      break;
+  }
+
+  [preferences setDocumentEdited:YES];
 }
 
 - (void)buttonsPressed:(id)sender
 {
-    switch ([[sender selectedCell] tag]) {
-        case 0:
-	    [self resetPreferences];
-            break;
-	case 1:
-	    [self setPreferences];
-	    break;
-    }
+  switch ([[sender selectedCell] tag]) {
+    case 0:
+      [self resetPreferences];
+      break;
+    case 1:
+      [self setPreferences];
+      break;
+  }
 
-    [preferences setDocumentEdited:NO];
-    //[preferences orderOut:self];
+  [preferences setDocumentEdited:NO];
+  //[preferences orderOut:self];
 }
 
 - (void)resetPreferences
 {
-    NSString *string;
+  NSString *string;
 
-    string = [[NSUserDefaults standardUserDefaults]  objectForKey:@"CacheSize"];
-    [cacheSizeField setStringValue:(string) ? string : @"50"];
+  string = [[NSUserDefaults standardUserDefaults] objectForKey:@"CacheSize"];
+  [cacheSizeField setStringValue:(string) ? string : @"50"];
 
-    [openRecursive setState:
-             ([[[NSUserDefaults standardUserDefaults] objectForKey:@"OpenRec"] 
-                            isEqualToString:@"YES"]) ? NSOnState : NSOffState];
+  [openRecursive setState:([[[NSUserDefaults standardUserDefaults] objectForKey:@"OpenRec"]
+                              isEqualToString:@"YES"])
+                              ? NSOnState
+                              : NSOffState];
 }
 
 - (void)setPreferences
 {
-    NSString *string = [prefDict objectForKey:@"CacheSize"];
+  NSString *string = [prefDict objectForKey:@"CacheSize"];
 
-    [[NSUserDefaults standardUserDefaults] setObject:string
-                                              forKey:@"CacheSize"];
-    [[ImageCache sharedCache] setMaxImages:[string intValue]];
+  [[NSUserDefaults standardUserDefaults] setObject:string forKey:@"CacheSize"];
+  [[ImageCache sharedCache] setMaxImages:[string intValue]];
 
-    string = [prefDict objectForKey:@"OpenRec"];
-    [[NSUserDefaults standardUserDefaults] setObject:string
-                                              forKey:@"OpenRec"];
+  string = [prefDict objectForKey:@"OpenRec"];
+  [[NSUserDefaults standardUserDefaults] setObject:string forKey:@"OpenRec"];
 }
 
 @end
