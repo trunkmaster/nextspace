46a42109d9e1ec62ba9c99f4ac5c55d7cbdb6486
Sergii Stoian <stoyan255@gmail.com>
2026-01-02 05:27:49 +0200

    freebsd-15.deps.sh: new file with dependecies for FreeBSD 15.

---
+++ Packaging/Sources/freebsd-15.deps.sh
@@ -0,0 +1,43 @@
+BUILD_TOOLS="
+    cmake
+    git
+    sudo
+"
+
+CORE_SYSTEM_DEPS="
+    libdispatch
+    icu
+    curl
+    libuuid
+    libinotify
+    libxml2
+    pkgconf
+"
+
+RUNTIME_DEPS="
+    xorg
+    xrandr
+"
+
+WRASTER_DEPS="
+  giflib
+  png
+  jbigkit
+  tiff
+  webp
+  libXpm
+  tiff
+"
+
+GNUSTEP_GUI_DEPS="libsndfile"
+
+BACK_ART_DEPS="
+  libart_lgpl
+"
+
+APPS_RUN_DEPS="
+ icu
+ urwfonts
+ noto-basic
+ noto-emoji
+"
