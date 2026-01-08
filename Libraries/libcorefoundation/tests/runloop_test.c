b3ef651f4e2a7dd292fd62217c53f09d8eab5644
Sergii Stoian <stoyan255@gmail.com>
2026-01-08 18:46:24 +0200

    libcorefoundation: update CFRunLoop test.

--- Libraries/libcorefoundation/tests/runloop_test.c
+++ Libraries/libcorefoundation/tests/runloop_test.c
@@ -2,52 +2,100 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreFoundation/CFFileDescriptor.h>
+#include <CoreFoundation/CFLogUtilities.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <X11/Xlib.h>
 
+// static void timerCB(CFRunLoopTimerRef timer, void *data)
+// {
+//   fprintf(stderr, "Timer was fired: %s runloop: %p\n", (char *)data, CFRunLoopGetCurrent());
+// }
+
+// static void setupRunLoop1(char *file)
+// {
+//   CFFileDescriptorRef fdRef;
+//   CFRunLoopSourceRef rlSource;
+//   int fd = open(file, O_RDONLY);
+
+//   fdRef = CFFileDescriptorCreate(kCFAllocatorDefault, fd, true, fdCallBack, NULL);
+//   CFFileDescriptorEnableCallBacks(fdRef, kCFFileDescriptorReadCallBack);
+
+//   rlSource = CFFileDescriptorCreateRunLoopSource(kCFAllocatorDefault, fdRef, 1);
+//   CFRunLoopAddSource(CFRunLoopGetCurrent(), rlSource, kCFRunLoopDefaultMode);
+//   CFRelease(rlSource);
+
+//   /* CFRunLoopTimerContext ctx = {0, "RunLoopTimer1", NULL, NULL, 0}; */
+//   /* CFRunLoopTimerRef timer =  CFRunLoopTimerCreate(kCFAllocatorDefault, */
+//   /*                                                 CFAbsoluteTimeGetCurrent() + 3, */
+//   /*                                                 0, 0, 0, timerCB, &ctx); */
+//   /* CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopDefaultMode); */
+//   /* CFRelease(timer); */
+
+//   /* CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false); */
+//   /* close(fd); */
+// }
+
+static int CantManageScreen = 0;
+static int _wmRunningErrorHandler(Display *dpy, XErrorEvent *error)
+{
+  CantManageScreen = 1;
+  return -1;
+}
+
+static Display *xDisplay = NULL;
+static long event_mask;
+static int xScreen = -1;
+
 static void fdCallBack(CFFileDescriptorRef fdref, CFOptionFlags callBackTypes, void *info)
 {
   int fd = CFFileDescriptorGetNativeDescriptor(fdref);
+  XEvent event;
 
   // take action on death of process here
   printf("callout on descriptor '%i'\n", fd);
-  
-  CFFileDescriptorInvalidate(fdref);
-  CFRelease(fdref); // the CFFileDescriptorRef is no longer of any use in this example
-  close(fd);
+
+  while (XPending(xDisplay) > 0) {
+    XNextEvent(xDisplay, &event);
+  }
+
+  CFFileDescriptorEnableCallBacks(fdref, kCFFileDescriptorReadCallBack);
+  // CFFileDescriptorInvalidate(fdref);
+  // CFRelease(fdref);  // the CFFileDescriptorRef is no longer of any use in this example
+  // close(fd);
 }
 
-static void timerCB(CFRunLoopTimerRef timer, void *data)
+static Bool startWindowManager(void)
 {
-  fprintf(stderr, "Timer was fired: %s runloop: %li\n",
-          (char *)data, CFRunLoopGetCurrent());
+  XErrorHandler oldHandler;
+
+  oldHandler = XSetErrorHandler((XErrorHandler)_wmRunningErrorHandler);
+  event_mask = SubstructureRedirectMask;
+
+  xDisplay = XOpenDisplay(NULL);
+  xScreen = DefaultScreen(xDisplay);
+  XSelectInput(xDisplay, RootWindow(xDisplay, xScreen), event_mask);
+
+  XSync(xDisplay, False);
+  XSetErrorHandler(oldHandler);
+
+  if (CantManageScreen) {
+    CFLog(kCFLogLevelError, CFSTR("%s: Can't open display"), __func__);
+    return False;
+  } else {
+    // XCloseDisplay(xDisplay);
+    return True;
+  }
 }
 
-static void setupRunLoop1(char *file)
+static void stopWindowManager(void)
 {
-  CFFileDescriptorRef fdRef;
-  CFRunLoopSourceRef  rlSource;
-  int fd = open(file, O_RDONLY);
-
-  fdRef = CFFileDescriptorCreate(kCFAllocatorDefault, fd, true, fdCallBack, NULL);
-  CFFileDescriptorEnableCallBacks(fdRef, kCFFileDescriptorReadCallBack);
-  
-  rlSource = CFFileDescriptorCreateRunLoopSource(kCFAllocatorDefault, fdRef, 1);
-  CFRunLoopAddSource(CFRunLoopGetCurrent(), rlSource, kCFRunLoopDefaultMode);
-  CFRelease(rlSource);
-
-  /* CFRunLoopTimerContext ctx = {0, "RunLoopTimer1", NULL, NULL, 0}; */
-  /* CFRunLoopTimerRef timer =  CFRunLoopTimerCreate(kCFAllocatorDefault, */
-  /*                                                 CFAbsoluteTimeGetCurrent() + 3, */
-  /*                                                 0, 0, 0, timerCB, &ctx); */
-  /* CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopDefaultMode); */
-  /* CFRelease(timer); */
-
-  /* CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false); */
-  /* close(fd); */
+  event_mask &= ~(SubstructureRedirectMask);
+  XSelectInput(xDisplay, RootWindow(xDisplay, xScreen), event_mask);
+  XSync(xDisplay, False);
+  XCloseDisplay(xDisplay);  
 }
 
 // one argument, an integer pid to watch, required
@@ -56,21 +104,28 @@ int main(int argc, char *argv[])
   CFFileDescriptorRef fdRef;
   CFRunLoopSourceRef rlSource;
   int fd;
-  Display *dpy = NULL;
-  
+  // Display *dpy = NULL;
+
   if (argc < 2) {
-    dpy = XOpenDisplay(getenv("DISPLAY"));
-    fd = ConnectionNumber(dpy);
-  }
-  else {
+    char *display_name = getenv("DISPLAY");
+    if (display_name == NULL) {
+
+    }
+    if (startWindowManager() == False) {
+      return 1;
+    }
+    fd = ConnectionNumber(xDisplay);
+  } else {
     fd = open(argv[1], O_RDONLY);
   }
-  
-  fprintf(stderr, "= START: setup runloop: %li (main: %li) FD: %i\n",
-          CFRunLoopGetCurrent(), CFRunLoopGetMain(), fd);
-  
+
+  fprintf(stderr, "= START: runloop setup: %p (main: %p) FD: %i\n", CFRunLoopGetCurrent(),
+          CFRunLoopGetMain(), fd);
+
+  // File descriptor
   fdRef = CFFileDescriptorCreate(kCFAllocatorDefault, fd, true, fdCallBack, NULL);
   CFFileDescriptorEnableCallBacks(fdRef, kCFFileDescriptorReadCallBack);
+  // Runloop
   rlSource = CFFileDescriptorCreateRunLoopSource(kCFAllocatorDefault, fdRef, 0);
   CFRunLoopAddSource(CFRunLoopGetCurrent(), rlSource, kCFRunLoopDefaultMode);
   CFRelease(rlSource);
@@ -82,24 +137,30 @@ int main(int argc, char *argv[])
   /* CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopDefaultMode); */
   /* CFRelease(timer); */
 
-  fprintf(stderr, "= FINISH: setup runloop: %li (main: %li)\n", CFRunLoopGetCurrent(), CFRunLoopGetMain());
-  
+  fprintf(stderr, "= FINISH: runloop setup: %p (main: %p)\n", CFRunLoopGetCurrent(),
+          CFRunLoopGetMain());
+
   // new thread
-  dispatch_queue_t wm_q = dispatch_queue_create("ns.workspace.wm", DISPATCH_QUEUE_CONCURRENT);
-  dispatch_async(wm_q, ^{
-      fprintf(stderr, "= START: setup runloop: %li (main: %li)\n", CFRunLoopGetCurrent(), CFRunLoopGetMain());
-      setupRunLoop1(argv[1]);
-      fprintf(stderr, "= FINISH: setup runloop: %li (main: %li)\n", CFRunLoopGetCurrent(), CFRunLoopGetMain());
-      CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false);
-      fprintf(stderr, "= QUIT: runloop: %li (main: %li)\n", CFRunLoopGetCurrent(), CFRunLoopGetMain());
-    });
+  // dispatch_queue_t wm_q = dispatch_queue_create("ns.workspace.wm", DISPATCH_QUEUE_CONCURRENT);
+  // dispatch_async(wm_q, ^{
+  //   fprintf(stderr, "= START: setup runloop: %p (main: %p)\n", CFRunLoopGetCurrent(),
+  //           CFRunLoopGetMain());
+  //   setupRunLoop1(argv[1]);
+  //   fprintf(stderr, "= FINISH: setup runloop: %p (main: %p)\n", CFRunLoopGetCurrent(),
+  //           CFRunLoopGetMain());
+  //   CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false);
+  //   fprintf(stderr, "= QUIT: runloop: %p (main: %p)\n", CFRunLoopGetCurrent(), CFRunLoopGetMain());
+  // });
 
   // run the run loop for 20 seconds
-  CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false);
+  // CFRunLoopRunInMode(kCFRunLoopDefaultMode, 6, false);
+  CFRunLoopRun();
+  // dispatch_main();
 
-  /* if (dpy) { */
-  /*   XCloseDisplay(dpy); */
-  /* } */
-  
+  // if (dpy) {
+  //   XCloseDisplay(dpy);
+  // }
+  stopWindowManager();
+
   return 0;
 }
